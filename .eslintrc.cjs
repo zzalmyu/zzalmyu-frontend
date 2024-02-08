e7f957d9f6097b6f6be3d212ea3e426376ca0fc8
J'aimemin <61978339+Kim-Jaemin420@users.noreply.github.com>
2024-02-08 20:59:38 +0900

    [#1] Chore: 프로젝트 세팅 (#2)

---
+++ .eslintrc.cjs
@@ -0,0 +1,88 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.html', '*.cjs', '*.mjs', '*.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'unused-imports', 'import'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'type',
+          'unknown',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/stores/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/apis/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/hooks/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/pages/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/components/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/assets/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@styled-system/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/utils/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/types/*',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+  },
+};
