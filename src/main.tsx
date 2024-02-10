75c7c1b28ea40ac2a64144ef3b81369071d01515
choi-ik <ci980704@naver.com>
2024-02-10 18:22:21 +0900

    Refactor: 전역적으로 사용되는 TanStack Router 타입을 react-router.d.ts 파일로 관리

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,6 @@ const router = createRouter({
   defaultPreload: "intent",
 });
 
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
