501333cceebe1ab4842f47f3c891b055d09064e7
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-02-12 13:27:52 +0900

    Feat: 테마 토글 Ui 구현

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,5 +1,5 @@
 import Logo from "@/assets/svg/logo.svg";
-
+import ThemeToggle from "@/components/Layout/Header/ThemeToggle";
 const Header = () => {
   return (
     <div className="navbar bg-background">
@@ -7,6 +7,7 @@ const Header = () => {
         <img src={Logo} alt="잘뮤니티 로고" className="h-10" />
       </a>
       <div className="flex flex-1 items-center justify-end space-x-2 px-2">
+        <ThemeToggle />
         <button className="btn h-9 min-h-9 border-primary bg-primary text-copy-cta">업로드</button>
         <div className="h-6 w-0.5 bg-gray-400"></div>
         <button className="btn btn-ghost h-6 min-h-9">로그인</button>
