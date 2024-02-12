6b3b4942ddf6c0603d135b2cd59620d7665ab91c
choi-ik <ci980704@naver.com>
2024-02-12 15:44:02 +0900

    Feat: 공통 이미지 컴포넌트에 Compound Component 패턴 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,18 @@ const App = () => {
   return (
     <Fragment>
       <div className="bg-primary text-copy-cta">hello react</div>
-      <AspectRatioImage src="https://jjalbang.today/files/jjalbox/2015/03/95_55169fb3a1f8b_1940.jpg" />
-      <AspectRatioImage src="https://jjalbang.today/files/jjalbox/2022/12/20221221_63a1db57406ff.png" />
+      <AspectRatioImage src="https://jjalbang.today/files/jjalbox/2015/03/95_55169fb3a1f8b_1940.jpg">
+        <AspectRatioImage.ButtonContainer>
+          <AspectRatioImage.LikeButton />
+          <AspectRatioImage.SendButton />
+        </AspectRatioImage.ButtonContainer>
+      </AspectRatioImage>
+      <AspectRatioImage src="https://jjalbang.today/files/jjalbox/2022/12/20221221_63a1db57406ff.png">
+        <AspectRatioImage.ButtonContainer>
+          <AspectRatioImage.LikeButton />
+          <AspectRatioImage.SendButton />
+        </AspectRatioImage.ButtonContainer>
+      </AspectRatioImage>
     </Fragment>
   );
 };
--- src/components/common/AspectRatioImage/AspectRatioImage.tsx
+++ src/components/common/AspectRatioImage/AspectRatioImage.tsx
@@ -1,15 +1,26 @@
+import { ReactNode } from "react";
+import ButtonContainer from "./ButtonContainer";
+import SendButton from "./SendButton";
+import LikeButton from "./LikeButton";
+
 interface Props {
+  children: ReactNode;
   src: string;
 }
 
-const AspectRatioImage = ({ src }: Props) => {
+const AspectRatioImage = ({ children, src }: Props) => {
   return (
     <div className="card card-compact relative mt-6 w-72 rounded-lg bg-base-100 shadow-xl">
+      {children}
       <figure>
         <img src={src} alt="zzal" className="h-full w-full rounded-lg object-cover" />
       </figure>
     </div>
   );
 };
+
+AspectRatioImage.ButtonContainer = ButtonContainer;
+AspectRatioImage.SendButton = SendButton;
+AspectRatioImage.LikeButton = LikeButton;
 
 export default AspectRatioImage;
