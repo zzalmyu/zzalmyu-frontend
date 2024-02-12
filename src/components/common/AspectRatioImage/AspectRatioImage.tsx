8417046029f3b9588b60c5a6efb983d6471d890f
choi-ik <ci980704@naver.com>
2024-02-12 15:16:33 +0900

    Feat: 이미지 hover시에 생성 될 보내기 버튼 생성

--- src/components/common/AspectRatioImage/AspectRatioImage.tsx
+++ src/components/common/AspectRatioImage/AspectRatioImage.tsx
@@ -1,3 +1,5 @@
+import SendButton from "./SendButton";
+
 interface Props {
   src: string;
 }
@@ -5,6 +7,7 @@ interface Props {
 const AspectRatioImage = ({ src }: Props) => {
   return (
     <div className="card card-compact mt-6 w-72 rounded-lg bg-base-100 shadow-xl">
+      <SendButton />
       <figure>
         <img src={src} alt="zzal" className="h-full w-full rounded-lg object-cover" />
       </figure>
---
+++ src/components/common/AspectRatioImage/SendButton.tsx
@@ -0,0 +1,20 @@
+const SendButton = () => {
+  return (
+    <svg width="35" height="35" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="25.5" cy="25.5" r="25.5" fill="#246FFF" />
+      <path
+        d="M19.1484 26L16 16C23.5313 18.1408 30.6333 21.5228 37 26C30.6337 30.4771 23.5321 33.8591 16.0012 36L19.1484 26ZM19.1484 26H27.7946H19.1484Z"
+        fill="white"
+      />
+      <path
+        d="M19.1484 26L16 16C23.5313 18.1408 30.6333 21.5228 37 26C30.6337 30.4771 23.5321 33.8591 16.0012 36L19.1484 26ZM19.1484 26H27.7946"
+        stroke="#11419E"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default SendButton;
