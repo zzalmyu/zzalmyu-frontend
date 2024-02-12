d83d3da0639021abe18a35dfa3c543aa291524d7
choi-ik <ci980704@naver.com>
2024-02-12 17:04:17 +0900

    Style: 공통 이미지 컴포넌트 hover 시에만 좋아요 및 전송 버튼이 나타나게 설정

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,12 @@ const App = () => {
     <Fragment>
       <div className="bg-primary text-copy-cta">hello react</div>
       <AspectRatioImage src="https://jjalbang.today/files/jjalbox/2015/03/95_55169fb3a1f8b_1940.jpg">
-        <AspectRatioImage.ButtonContainer>
-          <AspectRatioImage.LikeButton />
-          <AspectRatioImage.SendButton />
-        </AspectRatioImage.ButtonContainer>
+        <AspectRatioImage.LikeButton />
+        <AspectRatioImage.SendButton />
       </AspectRatioImage>
       <AspectRatioImage src="https://jjalbang.today/files/jjalbox/2022/12/20221221_63a1db57406ff.png">
-        <AspectRatioImage.ButtonContainer>
-          <AspectRatioImage.LikeButton />
-          <AspectRatioImage.SendButton />
-        </AspectRatioImage.ButtonContainer>
+        <AspectRatioImage.LikeButton />
+        <AspectRatioImage.SendButton />
       </AspectRatioImage>
     </Fragment>
   );
--- src/components/common/AspectRatioImage/AspectRatioImage.tsx
+++ src/components/common/AspectRatioImage/AspectRatioImage.tsx
@@ -9,8 +9,10 @@ interface Props {
 
 const AspectRatioImage = ({ children, src }: Props) => {
   return (
-    <div className="relative mt-6 w-72 rounded-lg bg-base-100 shadow-xl">
-      <div className="button-container absolute right-0 w-fit">{children}</div>
+    <div className="group relative mt-6 w-72 rounded-lg bg-base-100 shadow-xl">
+      <div className="button-container absolute right-2 top-1 z-10 w-fit opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100">
+        {children}
+      </div>
       <figure className="transition duration-300 ease-in-out hover:brightness-75">
         <img src={src} alt="zzal" className="h-full w-full rounded-lg object-cover" />
       </figure>
