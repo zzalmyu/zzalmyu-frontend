ab294f53d130cb239a096c068bb63fbd78b509e7
choi-ik <ci980704@naver.com>
2024-02-12 15:08:56 +0900

    Feat: 가로 길이를 기준으로 종횡비를 맞춘 이미지 컴포넌트 생성

---
+++ src/components/common/AspectRatioImage/AspectRatioImage.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  src: string;
+}
+
+const AspectRatioImage = ({ src }: Props) => {
+  return (
+    <div className="card card-compact mt-6 w-72 rounded-lg bg-base-100 shadow-xl">
+      <figure>
+        <img src={src} alt="zzal" className="h-full w-full rounded-lg object-cover" />
+      </figure>
+    </div>
+  );
+};
+
+export default AspectRatioImage;
