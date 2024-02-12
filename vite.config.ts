aa957b721bdca75128ace9913eba42f89c8129d5
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-02-12 17:28:28 +0900

    Chore: svg컴포넌트화를 위한 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      include: "*.svg",
-    }),
-  ],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
