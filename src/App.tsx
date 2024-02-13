29ade576e8e8cf0e767bd9f5973c4fe428d6fecb
choi-ik <ci980704@naver.com>
2024-02-14 01:04:44 +0900

    Cleanup: 공통 이미지 컴포넌트 test 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-import { Fragment } from "react";
-import ZzalCard from "@/components/common/ZzalCard";
-
 const App = () => {
-  const handleClick = () => {
-    console.log("보내기 및 좋아요 버튼 클릭!");
-  };
-
-  return (
-    <Fragment>
-      <div className="bg-primary text-copy-cta">hello react</div>
-      <ZzalCard
-        src="https://jjalbang.today/files/jjalbox/2022/12/20221221_63a1db57406ff.png"
-        alt="zzal"
-      >
-        <ZzalCard.LikeButton onClick={handleClick} isLiked={true} />
-        <ZzalCard.SendButton onClick={handleClick} />
-      </ZzalCard>
-    </Fragment>
-  );
+  return <div className="bg-primary text-copy-cta">hello react</div>;
 };
 
 export default App;
