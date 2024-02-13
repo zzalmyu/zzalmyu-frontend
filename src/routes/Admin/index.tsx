3f8d6bcfd9eb299129ae20d800835c3281125677
choi-ik <ci980704@naver.com>
2024-02-13 10:12:49 +0900

    Cleanup: 깃 캐시 삭제

--- src/routes/Admin/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-const Admin = () => {
-  return <div>관리자 페이지</div>;
-};
-
-export const Route = createFileRoute("/admin/")({
-  component: Admin,
-});
