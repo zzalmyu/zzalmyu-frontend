62289f2730f707f6223e5c76a121fc656bad8332
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-15 02:03:22 +0900

    Fix: ImageUpload 이벤트 에러 해결

--- src/components/UploadZzal/ImageUpload.tsx
+++ src/components/UploadZzal/ImageUpload.tsx
@@ -2,28 +2,29 @@ import { Fragment, useRef, useState } from "react";
 import { Upload } from "lucide-react";
 import ZzalCard from "../common/ZzalCard";
 
-const ImageUpload = () => {
-  const onChange = () => {
-    // handle file change
-  };
-  const [file, setFile] = useState<File | null>(null);
+interface Props {
+  onChange: (file: File | null) => void;
+}
+const ImageUpload = ({ onChange }: Props) => {
+  // const [file, setFile] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
   const [dragging, setDragging] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleClickDeleteButton = () => {
-    setFile(null);
+    onChange(null);
     setPreviewUrl(null);
   };
 
-  const handleFileChange = (e) => {
-    const files = e.target.files;
-    const changedFile = files[0];
-    setFile(changedFile);
-    if (changedFile) {
-      setPreviewUrl(URL.createObjectURL(changedFile));
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const files = event.target.files;
+      const changedFile = files[0];
+      onChange(changedFile);
+      if (changedFile) {
+        setPreviewUrl(URL.createObjectURL(changedFile));
+      }
     }
-    onChange && onChange(changedFile);
   };
 
   const handleChooseFile = () => {
@@ -32,46 +33,48 @@ const ImageUpload = () => {
     }
   };
 
-  const handleDragEnter = (e) => {
-    e.preventDefault();
+  const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
 
-    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+    if (event.dataTransfer.items && event.dataTransfer.items.length > 0) {
       setDragging(true);
     }
   };
-  const handleDragLeave = (e) => {
-    e.preventDefault();
+  const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
 
     setDragging(false);
   };
-  const handleDragOver = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
   };
-  const handleFileDrop = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+  const handleFileDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
 
-    const files = e.dataTransfer.files;
-    const changedFile = files[0];
-    setFile(changedFile);
-    if (changedFile) {
-      setPreviewUrl(URL.createObjectURL(changedFile));
+    if (event.dataTransfer) {
+      const files = event.dataTransfer.files;
+      const changedFile = files[0];
+      onChange(changedFile);
+      if (changedFile) {
+        setPreviewUrl(URL.createObjectURL(changedFile));
+      }
     }
-    onChange && onChange(changedFile);
+
     setDragging(false);
   };
 
   return (
     <div
-      className={`relative flex h-400pxr w-320pxr ${!file && "cursor-pointer"} flex-col items-center justify-center gap-50pxr overflow-clip rounded-[32px] border-4 border-dashed text-2xl font-bold ${dragging ? "border-primary text-primary" : "border-text-secondary text-text-secondary"} transition-colors hover:text-primary`}
+      className={`relative flex h-400pxr w-320pxr ${!previewUrl && "cursor-pointer"} flex-col items-center justify-center gap-50pxr overflow-clip rounded-[32px] border-4 border-dashed text-2xl font-bold ${dragging ? "border-primary text-primary" : "border-text-secondary text-text-secondary"} transition-colors hover:text-primary`}
       onClick={handleChooseFile}
       onDrop={handleFileDrop}
       onDragEnter={handleDragEnter}
       onDragLeave={handleDragLeave}
       onDragOver={handleDragOver}
     >
-      {file && (
+      {previewUrl && (
         <button
           className="absolute right-7pxr top-7pxr z-10 h-30pxr w-30pxr rounded-full bg-neutral pb-1pxr pl-1pxr text-base text-text-primary outline outline-transparent transition-[outline] hover:outline-delete"
           onClick={handleClickDeleteButton}
@@ -79,7 +82,7 @@ const ImageUpload = () => {
           ✕
         </button>
       )}
-      {!file ? (
+      {!previewUrl ? (
         <Fragment>
           <Upload aria-label="업로드 아이콘" size={72} />
           <div className={`hidden text-center sm:block`}>
