dca0938604733ba2382bd503c8ac7ef65d36574a
hy219 <jhy_0219@daum.net>
2024-02-15 04:19:03 +0900

    Feat: ReportModal UI 구현 및 Modal 공통 컴포넌트에 lg값 추가
    신고 이미지 상세 모달에서 보여줘야하는 요소에 맞게 모달 크기 확대
    Modal 공통 컴포넌트 재사용
    ZzalCard 공통 컴포넌트 재사용
    daisyUI 버튼 활용

--- src/components/Admin/ReportModal.tsx
+++ src/components/Admin/ReportModal.tsx
@@ -1,10 +1,55 @@
-import ReportingUserTable from "./ReportingUserTable";
+import { Trash2 } from "lucide-react";
+import Modal from "../common/Modal";
+import ZzalCard from "../common/ZzalCard";
 
-const ReportModal = () => {
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const ReportModal = ({ isOpen, onClose }: Props) => {
+  const reportData = [
+    { Date: "2024-02-08 23:03:20", UserEmail: "heejin1@asdf.com" },
+    { Date: "2024-02-07 10:01:00", UserEmail: "heejin1@asdf.com" },
+    { Date: "2024-02-06 10:01:00", UserEmail: "heejin2@asdf.com" },
+  ];
+
   return (
-    <div>
-      <ReportingUserTable />
-    </div>
+    <Modal isOpen={isOpen} onClose={onClose} size="lg">
+      <Modal.Body>
+        <div className="flex items-center">
+          <ZzalCard
+            src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F9928383A5BBECBB111"
+            alt="이미지가 올바르지 않습니다"
+            width={"2/5"}
+          />
+          <table className="table ml-5 w-3/5 bg-card">
+            <thead>
+              <tr>
+                <th className="text-center text-text-primary">신고된 날짜</th>
+                <th className="text-center text-text-primary">신고 사용자 이메일</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reportData.map((item, index) => {
+                return (
+                  <tr key={index} className="border-b-2 border-neutral-300 last:border-0">
+                    <td className="text-center font-bold">{item.Date.slice(0, 10)}</td>
+                    <td className="text-center font-bold">{item.UserEmail}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className="flex justify-center">
+          <button className="btn fixed bottom-10 w-48 rounded-full bg-delete hover:bg-delete hover:opacity-75">
+            <Trash2 color="#FFFF" strokeWidth={2} />
+            <span className="text-white">제거하기</span>
+          </button>
+        </div>
+      </Modal.Body>
+    </Modal>
   );
 };
 
--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -1,7 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useOverlay } from "@toss/use-overlay";
+import ReportModal from "@/components/admin/ReportModal";
 
 const Admin = () => {
-  return <div>관리자 페이지</div>;
+  const modalOverlay = useOverlay();
+
+  const handleClickModal = () => {
+    modalOverlay.open(({ isOpen, close }) => <ReportModal isOpen={isOpen} onClose={close} />);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClickModal}>상세보기</button>
+    </div>
+  );
 };
 
 export const Route = createFileRoute("/admin/")({
