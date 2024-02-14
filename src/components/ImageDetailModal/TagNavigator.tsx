a5a79e4c5ac56891e6aaabacb91e4b965e49ed36
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-02-15 03:52:23 +0900

    Feat: 이미지 상세 태그 렌더링 구현

---
+++ src/components/ImageDetailModal/TagNavigator.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+interface Props {
+  tags: string[];
+}
+
+const TagNavigator = ({ tags }: Props) => {
+  const MAX_TAG = 3;
+  const [currentGroup, setCurrentGroup] = useState(1);
+  const maxGroup = Math.ceil(tags.length / MAX_TAG);
+  const startIndex = (currentGroup - 1) * MAX_TAG;
+  const endIndex = startIndex + MAX_TAG;
+  const currentGroupTags = tags.slice(startIndex, endIndex);
+
+  const handleClickPrevious = () => setCurrentGroup((prevGroup) => prevGroup - 1);
+  const handleClickNext = () => setCurrentGroup((prevGroup) => prevGroup + 1);
+
+  const renderTagItems = () => (
+    <div className="flex flex-wrap justify-center">
+      {currentGroupTags.map((tag, index) => (
+        <span
+          key={index}
+          className=" badge-base badge-mg badge m-1  cursor-pointer bg-primary font-medium text-white hover:brightness-75"
+        >
+          {tag}
+        </span>
+      ))}
+    </div>
+  );
+
+  const renderIndicator = (
+    <div className="mt-2 flex justify-center">
+      {Array.from({ length: maxGroup }, (_, index) => (
+        <div
+          key={index}
+          className={`m-2 h-2 w-2 rounded-full bg-neutral ${currentGroup === index + 1 && "scale-150"}`}
+        />
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="relative">
+      <div className="flex items-center justify-center sm:h-12">
+        {currentGroup > 1 && (
+          <ChevronLeft onClick={handleClickPrevious} className="absolute left-2 cursor-pointer" />
+        )}
+        {renderTagItems()}
+        {currentGroup < maxGroup && (
+          <ChevronRight onClick={handleClickNext} className="absolute right-2 cursor-pointer" />
+        )}
+      </div>
+      {tags.length > MAX_TAG && renderIndicator}
+    </div>
+  );
+};
+
+export default TagNavigator;
--- src/components/ImageDetailModal/index.tsx
+++ src/components/ImageDetailModal/index.tsx
@@ -1,4 +1,5 @@
 import ImageController from "./ImageMenuBar";
+import TagNavigator from "./TagNavigator";
 import ZzalCard from "@/components/common/ZzalCard";
 import Modal from "@/components/common/Modal";
 interface Props {
@@ -9,10 +10,20 @@ interface Props {
 const ImageDetailModal = ({ isOpen, onClose }: Props) => {
   const src = "https://blog.kakaocdn.net/dn/bI2bwB/btsEP4W1m9P/os7ZrAK0fYhgOgBSKIiktk/img.png";
   const alt = "짤이미지";
-
+  const tags = [
+    "일이삼사오육칠팔구십",
+    "일이삼사오육칠팔구십",
+    "일이삼사오육칠팔구십",
+    "음식",
+    "여행",
+    "운동",
+    "영화",
+    "음악",
+  ];
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <Modal.Body>
+        <TagNavigator tags={tags} />
         <div className="my-10 flex justify-center">
           <div className="relative flex">
             <ZzalCard src={src} alt={alt} />
