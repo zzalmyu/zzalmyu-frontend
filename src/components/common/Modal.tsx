4689146539c501934d97b7bd7267040f1f3bd3aa
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-14 19:40:49 +0900

    Refactor: 타입 및 상수 위치 수정

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,24 +1,25 @@
 import { ReactNode } from "react";
 
+const MODAL_PADDING_VARIANTS = {
+  sm: "py-[1.5rem] px-[0.5rem]",
+  base: "p-[3rem]",
+};
+const MODAL_WIDTH_VARIANTS = {
+  sm: "w-[22rem] sm:w-[33rem]",
+  base: "w-[22rem] sm:w-[37rem]",
+};
+const MODAL_HEIGHT_VARIANTS = {
+  sm: "min-h-[15rem] sm:min-h-[15rem]",
+  base: "min-h-[23rem] sm:min-h-[23rem]",
+};
 interface Props {
   children: ReactNode;
   isOpen: boolean;
   onClose: () => void;
   size?: "sm" | "base";
 }
+
 const Modal = ({ children, isOpen, onClose, size = "base" }: Props) => {
-  const MODAL_PADDING_VARIANTS = {
-    sm: "py-[1.5rem] px-[0.5rem]",
-    base: "p-[3rem]",
-  };
-  const MODAL_WIDTH_VARIANTS = {
-    sm: "w-[22rem] sm:w-[33rem]",
-    base: "w-[22rem] sm:w-[37rem]",
-  };
-  const MODAL_HEIGHT_VARIANTS = {
-    sm: "min-h-[15rem] sm:min-h-[15rem]",
-    base: "min-h-[23rem] sm:min-h-[23rem]",
-  };
   return (
     // TODO: [2024.02.14] text-primary, text-secondary prettier 에러 해결
     isOpen && (
@@ -54,18 +55,21 @@ const ModalHeader = ({ hasCloseButton = false, children }: HeaderProps) => {
     </div>
   );
 };
+
 interface BodyProps {
   children: ReactNode;
 }
 const ModalBody = ({ children }: BodyProps) => {
   return <div>{children}</div>;
 };
+
 interface FooterProps {
   children: ReactNode;
 }
 const ModalFooter = ({ children }: FooterProps) => {
   return <div className="mt-8">{children}</div>;
 };
+
 Modal.Header = ModalHeader;
 Modal.Body = ModalBody;
 Modal.Footer = ModalFooter;
