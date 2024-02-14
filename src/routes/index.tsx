18546087e069fa47994f685e89a0ff6b877ea16b
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-14 16:39:10 +0900

    Test: alert 모달 테스트 코드 임시 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,59 @@
+import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
+import { useOverlay } from "@toss/use-overlay";
+import { MutationFunction } from "@tanstack/react-query";
+import AlertModal from "@/components/common/AlertModal";
 
 const Home = () => {
-  return <div>여긴 메인(홈) 페이지</div>;
+  // TODO: [2024.02.14] AlertModal 테스트 임시 코드 제거
+  const [id, setId] = useState("1");
+  interface DummyData {
+    id: string;
+    name: string;
+  }
+  interface DummyVariables {
+    userId: string;
+  }
+  const exampleMutationFunction: MutationFunction<DummyData, DummyVariables> = ({ userId }) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          id: "dummyId",
+          name: "dummy",
+        });
+        console.log(userId);
+      }, 2000);
+    });
+  };
+
+  const alertOverlay = useOverlay();
+  const handleClickAlert = () => {
+    alertOverlay.open(({ isOpen, close }) => (
+      <AlertModal
+        isOpen={isOpen}
+        onClose={close}
+        onDelete={exampleMutationFunction}
+        variables={{ userId: id }}
+      />
+    ));
+  };
+
+  return (
+    <>
+      <div>여긴 메인(홈) 페이지</div>
+      <div className="flex w-[10rem] flex-col gap-5">
+        <button
+          onClick={() => setId((prev) => prev + 1)}
+          className="rounded-xl border-2 border-primary "
+        >
+          click
+        </button>
+        <button onClick={handleClickAlert} className="rounded-xl border-2 border-primary ">
+          ALERT
+        </button>
+      </div>
+    </>
+  );
 };
 
 export const Route = createFileRoute("/")({
