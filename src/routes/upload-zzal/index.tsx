0e2f84eb06f1781aad7d79de167bd5f2c1c78ac5
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-15 02:06:00 +0900

    Feat: 짤 업로드 페이지 레이아웃 구현

--- src/routes/upload-zzal/index.tsx
+++ src/routes/upload-zzal/index.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
+import UploadGuide from "@/components/UploadZzal/UploadGuide";
+import ImageUpload from "@/components/UploadZzal/ImageUpload";
 
 const UploadZzal = () => {
-  return <div>짤 업로드 페이지</div>;
+  const [file, setFile] = useState<File | null>(null);
+
+  // TODO: [2024.02.15] file 테스트용 useEffect 코드 제거하기
+  useEffect(() => {
+    console.log(file);
+  }, [file]);
+
+  return (
+    <div className="flex flex-col items-center gap-20pxr px-50pxr pt-30pxr sm:px-100pxr">
+      <div className="self-start text-2xl font-extrabold text-text-primary">짤 업로드</div>
+      <UploadGuide />
+      <div className="mt-20pxr flex w-full flex-col items-center justify-center gap-50pxr sm:flex-row sm:items-start">
+        <ImageUpload onChange={(changedFile: File | null) => setFile(changedFile)} />
+        <div className="flex h-300pxr flex-1 flex-col justify-between">
+          <div className="w-full border-2 border-text-secondary">searchbar</div>
+          <button
+            className="h-40pxr w-100pxr self-center rounded-[100px] bg-primary font-bold text-white outline outline-2 outline-offset-2 outline-transparent hover:outline-yellow-500 sm:self-end"
+            onClick={() => {}}
+          >
+            업로드
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export const Route = createFileRoute("/upload-zzal/")({
