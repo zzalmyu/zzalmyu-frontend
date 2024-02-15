3530f0b3bef9ccd0ffe56a004e653751568a4290
Kim-Jaemin420 <jany240@naver.com>
2024-02-15 15:24:15 +0900

    Modify: 태그 초기화 버튼 아이콘 변경 및 스타일 수정

--- src/components/common/TagSearchForm.tsx
+++ src/components/common/TagSearchForm.tsx
@@ -52,26 +52,29 @@ const TagSearchForm = ({ className }: Props) => {
           </button>
         </div>
       </form>
-      <ul className="flex-column mt-4 flex min-h-8 flex-wrap items-center gap-2 pl-2">
-        {selectedTags.map((selectedTag, index) => (
-          <li
-            key={`${index}-${selectedTag}`}
-            className="mr-2 flex items-center rounded-3xl bg-tag px-2 py-1 text-white"
-          >
-            {selectedTag}
-            <XCircle
-              onClick={handleClickDeleteTag(selectedTag)}
-              aria-label="태그 삭제"
-              className="ml-1 w-5 cursor-pointer"
-            />
-          </li>
-        ))}
-      </ul>
-      {selectedTags.length > 0 && (
-        <button onClick={handleClickResetTagButton} className="flex-column mr-4 flex" type="button">
-          <RotateCw aria-label="태그 초기화" />
-        </button>
-      )}
+      <div className="flex items-center">
+        {selectedTags.length > 0 && (
+          <button onClick={handleClickResetTagButton} className=" mr-4 mt-4 pl-4" type="button">
+            <RotateCw aria-label="태그 초기화" />
+          </button>
+        )}
+
+        <ul className="flex-column mt-4 flex min-h-8 flex-wrap items-center justify-center gap-2 pl-1">
+          {selectedTags.map((selectedTag, index) => (
+            <li
+              key={`${index}-${selectedTag}`}
+              className="mr-2 flex items-center rounded-3xl bg-tag px-2 py-1 text-white"
+            >
+              {selectedTag}
+              <XCircle
+                onClick={handleClickDeleteTag(selectedTag)}
+                aria-label="태그 삭제"
+                className="ml-1 w-5 cursor-pointer"
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
