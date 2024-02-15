9f5ae251472611d53913868aca19ed097fe9e41d
J'aimemin <61978339+Kim-Jaemin420@users.noreply.github.com>
2024-02-15 16:13:19 +0900

    [#26] Feat: 태그 검색 컴포넌트 구현 (#31)

---
+++ src/components/common/TagBadge.tsx
@@ -0,0 +1,43 @@
+import { useAtom } from "jotai";
+import { cn } from "@/utils/tailwind";
+import { $selectedTags } from "@/store/tag";
+import { MAX_SEARCH_TAG } from "@/constants/tag";
+
+interface Props {
+  content: string;
+  isClickable?: boolean;
+  onClick?: () => void;
+  className?: string;
+}
+
+const TagBadge = ({ content, isClickable = false, className }: Props) => {
+  const [selectedTags, setSelectedTags] = useAtom($selectedTags);
+  const isSelectedTag = selectedTags.includes(content);
+
+  const handleClickTag = () => {
+    if (!isClickable) return;
+
+    if (selectedTags.includes(content)) {
+      setSelectedTags(selectedTags.filter((selectedTag) => selectedTag !== content));
+      return;
+    }
+
+    if (selectedTags.length < MAX_SEARCH_TAG) {
+      setSelectedTags((previousState) => [...previousState, content]);
+    }
+  };
+
+  const badgeClasses = cn("rounded-2xl px-4 py-2 text-white font-bold bg-tag", {
+    "cursor-pointer": isClickable,
+    "bg-gray-400": isClickable && !isSelectedTag,
+    "bg-tag": isSelectedTag,
+  });
+
+  return (
+    <span onClick={handleClickTag} className={cn(badgeClasses, className)}>
+      {content}
+    </span>
+  );
+};
+
+export default TagBadge;
