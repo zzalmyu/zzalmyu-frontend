98993aab48eacdf92661a2089b352d0db74574a7
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-15 20:17:38 +0900

    Refactor: AlertModal 이름 변경 및 컨벤션 정책대로 리팩토링

--- src/components/common/DeleteConfirmModal.tsx
+++ src/components/common/DeleteConfirmModal.tsx
@@ -1,11 +1,8 @@
+import { Fragment } from "react";
 import { AlertTriangle, Check, Trash2 } from "lucide-react";
 import { MutationFunction, useMutation } from "@tanstack/react-query";
+import { cn } from "@/utils/tailwind";
 import Modal from "@/components/common/modals/Modal";
-
-const ALERT_CONTENTS = {
-  main: "정말로 이 사진을 제거하시겠습니까?",
-  sub: "제거된 사진은 영구적으로 삭제됩니다",
-};
 
 interface Props<TData, TVariables> {
   isOpen: boolean;
@@ -14,7 +11,7 @@ interface Props<TData, TVariables> {
   variables: TVariables;
 }
 
-const AlertModal = <TData, TVariables>({
+const DeleteConfirmModal = <TData, TVariables>({
   isOpen,
   onClose,
   onDelete,
@@ -43,8 +40,10 @@ const AlertModal = <TData, TVariables>({
         <div className="flex flex-col items-center">
           <AlertTriangle color="#ED0000" strokeWidth="1.2" size="84" />
           <div className="mt-4 flex flex-col items-center gap-1">
-            <div className="text-xl font-extrabold sm:text-2xl">{ALERT_CONTENTS.main}</div>
-            <div className="font-bold text-text-secondary">{ALERT_CONTENTS.sub}</div>
+            <div className="text-xl font-extrabold sm:text-2xl">
+              정말로 이 사진을 제거하시겠습니까?
+            </div>
+            <div className="font-bold text-text-secondary">제거된 사진은 영구적으로 삭제됩니다</div>
           </div>
           <div className="mt-6 flex items-center gap-3 text-lg font-bold">
             <button
@@ -54,20 +53,28 @@ const AlertModal = <TData, TVariables>({
               취소
             </button>
             <button
-              className={`flex h-12 w-32 items-center justify-evenly rounded-[90px] ${status === "success" ? "bg-green-500 hover:outline-none" : "bg-delete hover:outline-delete"}  text-white outline outline-offset-2 outline-transparent transition-[outline_background-color]`}
+              className={cn(
+                status === "success"
+                  ? "bg-green-500 hover:outline-none"
+                  : "bg-delete hover:outline-delete",
+                `flex h-12 w-32 items-center justify-evenly rounded-[90px] text-white outline outline-offset-2 outline-transparent transition-[outline_background-color]`,
+              )}
               onClick={handleDelete}
               disabled={status !== "idle"}
             >
               {status === "idle" && (
-                <>
+                <Fragment>
                   <Trash2 color="#FFFF" strokeWidth={2} />
                   제거하기
-                </>
+                </Fragment>
               )}
               {status === "pending" && (
-                <span className="loading loading-spinner loading-xs text-white"></span>
+                <span
+                  className="loading loading-spinner loading-xs text-white"
+                  aria-label="제거 중"
+                />
               )}
-              {status === "success" && <Check />}
+              {status === "success" && <Check aria-label="제거 성공" />}
             </button>
           </div>
         </div>
@@ -76,4 +83,4 @@ const AlertModal = <TData, TVariables>({
   );
 };
 
-export default AlertModal;
+export default DeleteConfirmModal;
