b95842c0dc704dcf6970e4154f5249e80b8c78ed
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-15 13:49:09 +0900

    Feat: ModalContextProvider 생성 및 custom 훅 구현

---
+++ src/components/common/modals/ModalProvider.tsx
@@ -0,0 +1,14 @@
+import { ReactNode, createContext } from "react";
+
+export const ModalContext = createContext<(() => void) | undefined>(undefined);
+
+interface Props {
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const ModalProvider = ({ onClose, children }: Props) => {
+  return <ModalContext.Provider value={onClose}>{children}</ModalContext.Provider>;
+};
+
+export default ModalProvider;
