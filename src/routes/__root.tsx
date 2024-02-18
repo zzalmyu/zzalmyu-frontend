3ef6959031ad3f8ef042b15aa1e078275c7568e8
hy219 <jhy_0219@daum.net>
2024-02-19 01:27:22 +0900

    Style: 공통 layout 구현
    css screen 속성으로 뷰포트 비율에 맞춰 div 영역 지정
    뷰포트 : 해당 웹페이지를 실행하고 있는 기기의 화면크기
    헤더를 제외한 Outlet 영역에 전체 padding 10 지정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,8 +6,12 @@ import Header from "@/components/common/Header";
 const RootComponent = () => {
   return (
     <Fragment>
-      <Header />
-      <Outlet />
+      <div className="flex max-h-screen min-h-screen flex-col overflow-hidden pb-10">
+        <Header />
+        <div className="max-h-full min-h-full overflow-auto px-10 pt-10">
+          <Outlet />
+        </div>
+      </div>
     </Fragment>
   );
 };
