4b9f3c58d6379fd160dddcf15179557af1f242f9
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-19 20:06:11 +0900

    Feat: useAnimation 훅 구현 및 chat layout에 적용

--- src/routes/_chat-layout.tsx
+++ src/routes/_chat-layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createFileRoute } from "@tanstack/react-router";
 import { useAtomValue } from "jotai";
+import useAnimation from "@/hooks/useAnimation";
 import { cn } from "@/utils/tailwind";
 import { $isChatOpen } from "@/store/chat";
 import ChatToggleButton from "@/components/chat/ChatToggleButton";
@@ -10,18 +11,24 @@ export const Route = createFileRoute("/_chat-layout")({
 
 function ChatLayoutComponent() {
   const isChatOpen = useAtomValue($isChatOpen);
-
+  const [shouldRenderCategory, handleTransitionEnd, triggerAnimation] = useAnimation(isChatOpen);
   return (
     <>
-      <div className="flex flex-1 items-stretch">
-        <section className="relative flex-1">
+      <div className="flex flex-1 items-stretch transition duration-500">
+        <section className="relative w-full flex-1 border-2 border-green-500 ">
           <div className="absolute bottom-15pxr right-15pxr">
             <ChatToggleButton />
           </div>
           <Outlet />
         </section>
-        {isChatOpen && (
-          <section className={cn("w-450pxr bg-secondary p-20pxr")}>
+        {shouldRenderCategory && (
+          <section
+            onTransitionEnd={handleTransitionEnd}
+            className={cn(
+              "flex-[0.6] bg-secondary p-20pxr transition-[all] duration-500",
+              !triggerAnimation && " flex-[0.000001]",
+            )}
+          >
             <div className="h-full w-full rounded-16pxr bg-background"></div>
           </section>
         )}
