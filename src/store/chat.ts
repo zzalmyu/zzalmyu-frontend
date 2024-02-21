38cfa2685d403a97785cd7c690f734eb09e16e60
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-21 18:54:51 +0900

    Feat: messagePreview 아톰 정의

--- src/store/chat.ts
+++ src/store/chat.ts
@@ -1,3 +1,7 @@
 import { atom } from "jotai";
 
 export const $isChatOpen = atom(false);
+
+export const $messagePreview = atom({
+  src: "https://i.pinimg.com/564x/bb/26/c6/bb26c6670b60beff3d81ef74771f2c69.jpg",
+});
