5a19d4aa12fbb8474b86645e484b3d4c9987da7c
Choi-ik <ci980704@naver.com>
2024-02-21 10:06:08 +0900

    [#52] Feat: 태그 '검색 결과가 없음'을 나타내는 컴포넌트 구현 (#53)

---
+++ src/components/common/NoSearchResults.tsx
@@ -0,0 +1,23 @@
+import { SearchX } from "lucide-react";
+
+interface Props {
+  onClick: (isTagReset: boolean) => void;
+}
+
+const NoSearchResults = ({ onClick }: Props) => {
+  return (
+    <div className="flex h-400pxr w-[35%] min-w-96 flex-col items-center rounded-xl bg-card">
+      <SearchX aria-label="검색 실패" size={60} className="mt-80pxr" />
+      <p className="mt-35pxr text-4xl">검색 결과가 없어요!</p>
+      <p className="mt-20pxr text-text-secondary">필터를 삭제하고 다시 시도해보세요.</p>
+      <button
+        className="mt-40pxr h-40pxr w-180pxr rounded-full bg-primary text-white"
+        onClick={() => onClick(true)}
+      >
+        되돌아가기
+      </button>
+    </div>
+  );
+};
+
+export default NoSearchResults;
