591a9898d2c58cb8492c1be4f85e87c4af078747
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-02-22 17:21:52 +0900

    Refactor: import 코드 이후 줄바꿈 추가

--- src/types/report.dto.ts
+++ src/types/report.dto.ts
@@ -1,3 +1,4 @@
 import { Report, ReportDetail } from "./report";
+
 export type GetReportResponse = Report[];
 export type GetReportDetailResponse = ReportDetail[];
--- src/types/report.ts
+++ src/types/report.ts
@@ -3,6 +3,7 @@ export interface Report {
   lastReportAt: string;
   reportCount: number;
   tags: { tagId: number; tagName: string }[];
+  // TODO: [2024.02.22] 추후에 tags 파일에서 타입 가줘오기
 }
 export interface ReportDetail {
   reportDate: string;
