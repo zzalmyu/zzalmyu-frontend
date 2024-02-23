933fa0b0aa3c1603d0ceb5930692c50b7b26b07a
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-23 21:27:25 +0900

    Refactor: Message의 isUser 속성 isMyMessage로 수정

--- src/components/common/Chat/ChatSection.tsx
+++ src/components/common/Chat/ChatSection.tsx
@@ -5,35 +5,31 @@ import Peek from "./Peek";
 const DUMMY_MESSAGES = [
   {
     src: "https://i.pinimg.com/564x/bb/26/c6/bb26c6670b60beff3d81ef74771f2c69.jpg",
-    isUser: false,
+    isMyMessage: false,
   },
   {
     src: "https://i.pinimg.com/564x/bb/26/c6/bb26c6670b60beff3d81ef74771f2c69.jpg",
-    isUser: false,
+    isMyMessage: false,
   },
   {
     src: "https://i.pinimg.com/564x/bb/26/c6/bb26c6670b60beff3d81ef74771f2c69.jpg",
-    isUser: true,
+    isMyMessage: true,
   },
   {
     src: "https://i.pinimg.com/564x/bb/26/c6/bb26c6670b60beff3d81ef74771f2c69.jpg",
-    isUser: false,
+    isMyMessage: false,
   },
 ];
 
 interface Props {
   setScrollPosition: () => void;
 }
+
 const ChatSection = forwardRef<HTMLDivElement, Props>(({ setScrollPosition }, ref) => {
-  // const chatRoomRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    // if (!chatRoomRef.current) return;
-    // const top = sessionStorage.getItem("sidebar-scroll");
-    // if (top !== null) {
-    //   chatRoomRef.current.scrollTop = parseInt(top, 10);
-    // }
     setScrollPosition();
   }, [setScrollPosition]);
+
   return (
     <section className="relative h-full w-full bg-secondary p-20pxr ">
       <div
@@ -41,8 +37,8 @@ const ChatSection = forwardRef<HTMLDivElement, Props>(({ setScrollPosition }, re
         className="relative h-full w-full overflow-y-auto rounded-16pxr bg-background pb-15pxr"
       >
         <div className="flex flex-1 flex-col ">
-          {DUMMY_MESSAGES.map(({ src, isUser }, index) => (
-            <Message key={`${index}-${src}`} src={src} isUser={isUser} />
+          {DUMMY_MESSAGES.map(({ src, isMyMessage }, index) => (
+            <Message key={`${index}-${src}`} src={src} isMyMessage={isMyMessage} />
           ))}
         </div>
       </div>
--- src/components/common/Chat/Message.tsx
+++ src/components/common/Chat/Message.tsx
@@ -3,13 +3,13 @@ import ZzalCard from "../ZzalCard";
 
 interface Props {
   src: string;
-  isUser: boolean;
+  isMyMessage: boolean;
 }
-const Message = ({ src, isUser }: Props) => {
+const Message = ({ src, isMyMessage }: Props) => {
   return (
     <div
       className={cn(
-        isUser ? "self-end" : "self-start",
+        isMyMessage ? "self-end" : "self-start",
         "mx-15pxr my-5pxr w-1/3 rounded-xl bg-surface1 p-5pxr",
       )}
     >
