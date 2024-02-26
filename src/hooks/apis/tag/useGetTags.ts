e32ae4d8fd494b85badac93036ef42afafe5d8a7
choi-ik <ci980704@naver.com>
2024-02-26 09:27:17 +0900

    Feat: 검색한 텍스트에 의해 자동완성된 UI를 불러올 useQuery 구현

---
+++ src/hooks/apis/tag/useGetTags.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { getSearchTag } from "@/apis/tag";
+
+export const useGetTags = (tag: string) => {
+  return useQuery({
+    queryKey: ["tag", tag] as const,
+    queryFn: () => getSearchTag(tag),
+    refetchOnWindowFocus: false,
+  });
+};
