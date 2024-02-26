3d27e55fdb3c540034975baf19fe89f24530774f
Kim-Jaemin420 <jany240@naver.com>
2024-02-26 11:35:45 +0900

    Feat: masonry layout 적용

--- src/routes/my-liked-zzal/index.tsx
+++ src/routes/my-liked-zzal/index.tsx
@@ -4,6 +4,7 @@ import Pending from "./MyLikedZzal.pendingComponent";
 import useGetMyLikedZzals from "@/hooks/api/zzal/useGetMyLikedZzals";
 import ZzalCard from "@/components/common/ZzalCard";
 import useIntersectionObserver from "@/hooks/common/useIntersectionObserver";
+import MasonryLayout from "@/components/common/MasonryLayout";
 
 const MyLikedZzal = () => {
   const fetchMoreRef = useRef(null);
@@ -17,14 +18,12 @@ const MyLikedZzal = () => {
   return (
     <div className="p-4 text-center">
       <h1 className="mb-4 text-2xl font-bold">좋아요 한 짤 페이지</h1>
-      <div className="mx-auto sm:max-w-620pxr sm:columns-2 md:max-w-920pxr md:columns-3 lg:max-w-1220pxr lg:columns-4">
+      <MasonryLayout>
         {zzals.map(({ imageId, path, title }) => (
-          <div key={imageId} className="mb-4 inline-block break-inside-avoid">
-            <ZzalCard src={path} alt={title} />
-          </div>
+          <ZzalCard key={imageId} src={path} alt={title} />
         ))}
-        <div ref={fetchMoreRef} />
-      </div>
+      </MasonryLayout>
+      <div ref={fetchMoreRef} />
     </div>
   );
 };
