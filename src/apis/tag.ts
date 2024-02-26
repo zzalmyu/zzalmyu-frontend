ed8cb05ddc9fcb72e3590a8c43d34d21fe7d2b0b
choi-ik <ci980704@naver.com>
2024-02-27 01:43:18 +0900

    Rename: baseUrl과 endPoint 변경

--- src/apis/tag.ts
+++ src/apis/tag.ts
@@ -3,7 +3,7 @@ import { GetTagsResponse } from "@/types/tag.dto";
 
 export const getSearchTag = (tag: string) =>
   http.get<GetTagsResponse>({
-    url: "/api/v1/tag/search",
+    url: "/v1/tag/search",
     params: {
       input: tag,
     },
