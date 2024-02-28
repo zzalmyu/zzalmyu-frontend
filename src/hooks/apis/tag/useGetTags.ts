708e448c17669034466036c6ce6f4591cec02705
choi-ik <ci980704@naver.com>
2024-02-28 09:27:07 +0900

    Refactor: useQuery 함수 select 메소드 추가 -> 받아오는 전체 길이의 자동완성 태그를 5개 까지로 제한

--- src/hooks/apis/tag/useGetTags.ts
+++ src/hooks/apis/tag/useGetTags.ts
@@ -2,9 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import { getSearchTag } from "@/apis/tag";
 
 export const useGetTags = (tag: string) => {
+  const MAX_TAG_RESPONSE_COUNT = 5;
+
   return useQuery({
     queryKey: ["tag", tag] as const,
     queryFn: () => getSearchTag(tag),
-    enabled: false,
+    select: (tags) => {
+      const tagsResponse = tags.filter((_, index) => index < MAX_TAG_RESPONSE_COUNT);
+      return tagsResponse;
+    },
+    enabled: !!tag,
   });
 };
