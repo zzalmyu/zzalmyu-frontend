bb3ddb822a755d4d4fd2384c7bfe3fe5c80438c6
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-02-28 10:14:34 +0900

    Refactor: formateDate util 함수 반환 형식 수정

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,9 @@
 export const formatDate = (dateString: string) => {
   const date = new Date(dateString);
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
 };
