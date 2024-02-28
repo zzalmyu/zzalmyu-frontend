a9fc448761a0d73c73c1590b1fae69b70c663e4b
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-02-28 09:26:14 +0900

    Feat: 관리자 사진 삭제 훅, 실패 toast에 재시도 버튼 추가 및 코드 정리

--- src/hooks/api/report/useDeleteReportedImage.tsx
+++ src/hooks/api/report/useDeleteReportedImage.tsx
@@ -2,32 +2,33 @@ import { toast } from "react-toastify";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "@tanstack/react-router";
 import { deleteReportedImage } from "@/apis/report";
-import { Report } from "@/types/report";
 
 const useDeleteReportedImage = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate({ from: "/admin/reports/$imageId" });
 
+  const handleRetry = (imageId: string) => () => {
+    mutate(imageId);
+  };
+
+  const DeleteFailureToast = ({ imageId }: { imageId: string }) => (
+    <div>
+      <div>사진 삭제에 실패했습니다.</div>
+      <button className="btn btn-outline btn-error btn-xs" onClick={handleRetry(imageId)}>
+        재시도
+      </button>
+    </div>
+  );
+
   const { mutate, ...rest } = useMutation({
     mutationFn: deleteReportedImage,
-    onSuccess: (imageId) => {
-      queryClient.setQueryData(
-        ["reports"],
-        ({ pages, pageParams }: { pages: Report[][]; pageParams: number[] }) => {
-          const updatedPages = pages.map((page) =>
-            page.filter((report) => report.imageId !== imageId),
-          );
-
-          return { pages: updatedPages, pageParams };
-        },
-      );
-
+    onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["reports"] });
       toast.success("사진이 삭제되었습니다");
       navigate({ to: "/admin/reports" });
     },
-    onError: () => {
-      toast.error("사진 삭제에 실패했습니다.");
+    onError: (imageId) => {
+      toast.error(<DeleteFailureToast imageId={imageId.toString()} />);
     },
   });
 
