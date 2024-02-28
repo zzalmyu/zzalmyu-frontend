fa351b0bce213b2c67d602aa49e92dc382a0b1a4
hy219 <jhy_0219@daum.net>
2024-02-28 23:12:10 +0900

    Refactor: api 요청 response 타입 명시
    response 데이터가 없어서, void로 명시

--- src/apis/report.ts
+++ src/apis/report.ts
@@ -2,7 +2,7 @@ import { PostReportZzalRequeust } from "@/types/report.dto";
 import http from "./core";
 
 export const postReportZzal = async ({ imageId }: PostReportZzalRequeust) => {
-  const response = await http.post({
+  const response = await http.post<void>({
     url: `/report/${imageId}`,
   });
 
