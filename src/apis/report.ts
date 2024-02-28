f7ee5c8da2c5da48b0119dd5861b4f17515f9cb1
hy219 <jhy_0219@daum.net>
2024-02-28 22:29:33 +0900

    Refactor: POST 요청에 대한 응답 타입 삭제 및 비동기 처리 추가
    POST 요청에 대한 응답 타입이 존재하지 않으므로 타입 삭제
    POST 요청에 비동기 처리를 하여 응답 대기 시간 단축

--- src/apis/report.ts
+++ src/apis/report.ts
@@ -1,8 +1,10 @@
 import { PostReportZzalRequeust } from "@/types/report.dto";
 import http from "./core";
 
-export const postReportZzal = ({ imageId }: PostReportZzalRequeust) => {
-  return http.post<PostReportZzalRequeust>({
+export const postReportZzal = async ({ imageId }: PostReportZzalRequeust) => {
+  const response = await http.post({
     url: `/report/${imageId}`,
   });
+
+  return response;
 };
