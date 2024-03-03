d527ac1e49c38a6b79be68ecee677e91dbfaf2b3
hy219 <jhy_0219@daum.net>
2024-03-03 15:47:03 +0900

    Feat: 짤 이미지 삭제를 성공/실패 했을 때 toast 및 동작 구현
    toastify 사용
    짤 이미지 삭제 성공 시 모달 자동으로 닫히도록 구현
    TODO: 짤 이미지 삭제 성공 시 페이지 새로고침
    TODO: 이미지 상세 모달 구현 시, 유저 본인이 업로드한 이미지가 아닐 경우 삭제 버튼 비활성화 구현 필요

--- src/hooks/api/report/useDeleteMyZzal.ts
+++ src/hooks/api/report/useDeleteMyZzal.ts
@@ -1,9 +1,20 @@
+import { toast } from "react-toastify";
 import { useMutation } from "@tanstack/react-query";
 import { deleteMyZzal } from "@/apis/zzal";
+import useModalContext from "@/hooks/modals/useModalContext";
 
 const useDeleteMyZzal = () => {
+  const onClose = useModalContext();
+
   const { mutate, ...rest } = useMutation({
     mutationFn: deleteMyZzal,
+    onSuccess: () => {
+      toast.success("사진이 삭제되었습니다.");
+      onClose();
+    },
+    onError: () => {
+      toast.error("사진 삭제에 실패했습니다.");
+    },
   });
   return { deleteMyZzal: mutate, ...rest };
 };
--- src/routes/_layout-with-chat/index.tsx
+++ src/routes/_layout-with-chat/index.tsx
@@ -1,7 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useOverlay } from "@toss/use-overlay";
+import ImageDetailModal from "@/components/ImageDetailModal";
 
 const Home = () => {
-  return <div></div>;
+  // TODO: [2024-03-03] 이미지 상세 모달 구현 코드에 따라 변경될 수 있습니다. (이미지 삭제 성공 시, 모달이 닫히는 동작을 구현하기 위해 코드 구현)
+  const zzalModalOverlay = useOverlay();
+
+  const handleClickZzal = () => {
+    zzalModalOverlay.open(({ isOpen, close }) => (
+      <ImageDetailModal isOpen={isOpen} onClose={close} />
+    ));
+  };
+
+  return (
+    <div>
+      <button onClick={handleClickZzal}>Zzal 클릭 시 상세 모달 열기</button>
+    </div>
+  );
 };
 
 export const Route = createFileRoute("/_layout-with-chat/")({
