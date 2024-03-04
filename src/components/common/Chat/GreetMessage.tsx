eedb4168ea08329f033149c99f821b98db68b012
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-03-04 14:01:23 +0900

    Feat: 입장 메세지 컴포넌트 구현

---
+++ src/components/common/Chat/GreetMessage.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  message: string;
+}
+const GreetMessage = ({ message }: Props) => {
+  return (
+    <div className="mx-auto my-5pxr rounded-full bg-gray-300 px-2 py-1 text-center text-sm font-semibold text-neutral">
+      🤚 {message}
+    </div>
+  );
+};
+
+export default GreetMessage;
