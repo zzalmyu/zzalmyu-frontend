d1bc17a293dc74c45ce0a6b0a1e9dc60c30dee07
Kim-Jaemin420 <jany240@naver.com>
2024-03-04 20:07:15 +0900

    Modify: api url에 v1 추가

--- src/apis/zzal.ts
+++ src/apis/zzal.ts
@@ -4,4 +4,4 @@ import http from "./core";
 const SIZE = 10;
 
 export const getMyLikedZzals = (offset: number) =>
-  http.get<GetMyLikedZzalsResponse>({ url: `/image/like?page=${offset}&size=${SIZE}` });
+  http.get<GetMyLikedZzalsResponse>({ url: `v1/image/like?page=${offset}&size=${SIZE}` });
