2ca61837f3f2c4b4ec613334866a4d5f4f829088
Kim-Jaemin420 <jany240@naver.com>
2024-03-04 11:33:05 +0900

    Feat: 많이 사용한 태그 api 및 커스텀 훅 구현

--- src/apis/tag.ts
+++ src/apis/tag.ts
@@ -1,5 +1,5 @@
 import http from "@/apis/core";
-import { GetTagsResponse } from "@/types/tag.dto";
+import { GetTagsResponse, GetTopTagsFromUploadedResponse } from "@/types/tag.dto";
 
 export const getSearchTag = (tag: string) =>
   http.get<GetTagsResponse>({
@@ -7,4 +7,9 @@ export const getSearchTag = (tag: string) =>
     params: {
       keyword: tag,
     },
+  });
+
+export const getTopTagsFromUploaded = () =>
+  http.get<GetTopTagsFromUploadedResponse>({
+    url: "/v1/tag/me/upload",
   });
