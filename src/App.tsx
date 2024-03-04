fc8fa3ce02bb087f5dcaad9c99a44a30097d1438
Kim-Jaemin420 <jany240@naver.com>
2024-03-04 12:05:58 +0900

    Feat: type 선언 및 devtools 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { OverlayProvider } from "@toss/use-overlay";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
 });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const queryClient = new QueryClient();
 
@@ -16,6 +24,8 @@ const App = () => {
       <OverlayProvider>
         <RouterProvider router={router} />
       </OverlayProvider>
+      <ReactQueryDevtools buttonPosition="top-right" />
+      <TanStackRouterDevtools position="bottom-right" />
     </QueryClientProvider>
   );
 };
