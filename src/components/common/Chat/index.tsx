0f8a44d621769a7e4275fb326ec528fc0f67b178
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-03-06 01:08:27 +0900

    Modify: Chat 로직 layout에서 분리

--- src/components/common/Chat/index.tsx
+++ src/components/common/Chat/index.tsx
@@ -1,7 +1,12 @@
-import { useEffect, useRef } from "react";
+import { Fragment, useEffect, useRef } from "react";
+import { useAtomValue } from "jotai";
 import { getSessionStorage } from "@/utils/sessionStorage";
+import { cn } from "@/utils/tailwind";
 import MessagePeek from "./MessagePeek";
 import Message from "./Message";
+import ChatCloseButton from "./ChatCloseButton";
+import ChatToggleButton from "./ChatToggleButton";
+import { $isChatOpen } from "@/store/chat";
 
 // TODO: [2024.02.23] 더미 데이터 제거 및 WS 연결
 const DUMMY_MESSAGES = [
@@ -28,34 +33,57 @@ const DUMMY_MESSAGES = [
 ];
 
 const Chat = () => {
+  const isChatOpen = useAtomValue($isChatOpen);
   const chatRoomRef = useRef<HTMLDivElement>(null);
 
-  const setScrollPosition = () => {
-    if (!chatRoomRef.current) return;
-    const top = getSessionStorage("sidebar-scroll");
-    if (top !== null) {
-      chatRoomRef.current.scrollTop = parseInt(top, 10);
-    }
-  };
-
   useEffect(() => {
+    const setScrollPosition = () => {
+      if (!chatRoomRef.current) return;
+      const top = getSessionStorage("sidebar-scroll");
+      if (top !== null) {
+        chatRoomRef.current.scrollTop = parseInt(top, 10);
+      }
+    };
+
     setScrollPosition();
-  }, [setScrollPosition]);
+  }, []);
 
   return (
-    <section className="relative h-full w-full">
+    <Fragment>
       <div
-        ref={chatRoomRef}
-        className="relative h-full w-full overflow-y-auto rounded-16pxr bg-background pb-15pxr"
+        className={cn(
+          "absolute top-0 z-10 flex h-60pxr items-center justify-between border border-border bg-background px-4 transition-[width] duration-500 ease-in-out",
+          isChatOpen ? "w-[70%]" : "w-full",
+        )}
       >
-        <div className="flex flex-1 flex-col ">
-          {DUMMY_MESSAGES.map(({ src, isMyMessage }, index) => (
-            <Message key={`${index}-${src}`} src={src} isMyMessage={isMyMessage} />
-          ))}
-        </div>
+        <div className="text-xl font-bold text-text-primary">TITLE</div>
+        <ChatToggleButton />
       </div>
-      <MessagePeek />
-    </section>
+      <div
+        className={cn(
+          "absolute right-0 h-full w-[30%] border border-l-0 border-border transition-[opacity_transform] duration-500 ease-in-out",
+          isChatOpen ? "opacity-100" : "translate-x-full opacity-0",
+        )}
+      >
+        <div className="sticky left-0 right-0 top-0 flex h-60pxr flex-1 items-center justify-between border-b border-border px-4">
+          <div className="text-xl font-bold text-text-primary">고독한 채팅방</div>
+          <ChatCloseButton />
+        </div>
+        <section className="relative h-full w-full">
+          <div
+            ref={chatRoomRef}
+            className="relative h-full w-full overflow-y-auto rounded-16pxr bg-background pb-15pxr"
+          >
+            <div className="flex flex-1 flex-col ">
+              {DUMMY_MESSAGES.map(({ src, isMyMessage }, index) => (
+                <Message key={`${index}-${src}`} src={src} isMyMessage={isMyMessage} />
+              ))}
+            </div>
+          </div>
+          <MessagePeek />
+        </section>
+      </div>
+    </Fragment>
   );
 };
 
