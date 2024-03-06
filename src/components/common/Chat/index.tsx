b94a3c14ef5ee998bf9f1aa66f0ef1336549b9fb
Yongjae Choi <yongjae.choi20@gmail.com>
2024-03-06 10:28:13 +0900

    [#79] Feat: 채팅 기능 연결 (#96)

--- src/components/common/Chat/index.tsx
+++ src/components/common/Chat/index.tsx
@@ -1,58 +1,33 @@
-import { Fragment, useEffect, useRef } from "react";
+import { Fragment, useRef } from "react";
 import { useAtom } from "jotai";
 import { X } from "lucide-react";
-import { getSessionStorage } from "@/utils/sessionStorage";
 import { cn } from "@/utils/tailwind";
 import MessagePeek from "./MessagePeek";
-import Message from "./Message";
+import ZzalMessage from "./ZzalMessage";
+import GreetMessage from "./GreetMessage";
 import { $isChatOpen } from "@/store/chat";
-
-// TODO: [2024.02.23] 더미 데이터 제거 및 WS 연결
-const DUMMY_MESSAGES = [
-  {
-    src: "https://i.pinimg.com/564x/93/66/aa/9366aa85149388c2e1f09bbf9d79b60e.jpg",
-    isMyMessage: false,
-  },
-  {
-    src: "https://i.pinimg.com/564x/dc/77/a8/dc77a8b6e9dd0a4a17470a0e2f85cd9f.jpg",
-    isMyMessage: false,
-  },
-  {
-    src: "https://i.pinimg.com/564x/bb/26/c6/bb26c6670b60beff3d81ef74771f2c69.jpg",
-    isMyMessage: true,
-  },
-  {
-    src: "https://jjalbang.today/files/jjalbox/2019/01/20190120_5c435f3d5c0d4.jpg",
-    isMyMessage: false,
-  },
-  {
-    src: "https://jjalbang.today/files/jjalbox/2021/06/20210608_60bf89513e1ea.jpg",
-    isMyMessage: false,
-  },
-];
+import useChat from "@/hooks/chat/useChat";
 
 const Chat = () => {
-  const [isChatOpen, setIsChatOpen] = useAtom($isChatOpen);
   const chatRoomRef = useRef<HTMLDivElement>(null);
+  const [isChatOpen, setIsChatOpen] = useAtom($isChatOpen);
+
+  const setScrollToBottom = () => {
+    if (!chatRoomRef.current) return;
+    chatRoomRef.current.scrollTo(0, chatRoomRef.current.scrollHeight);
+  };
+
+  const { handleSendMessage, messages } = useChat(setScrollToBottom);
+
+  const handleClickSend = () => handleSendMessage("zzal");
 
   const handleClickChatCloseButton = () => {
     setIsChatOpen(false);
   };
+
   const handleClickChatToggleButton = () => {
     setIsChatOpen((prev) => !prev);
   };
-
-  useEffect(() => {
-    const setScrollPosition = () => {
-      if (!chatRoomRef.current) return;
-      const top = getSessionStorage("chat-room-scroll");
-      if (top !== null) {
-        chatRoomRef.current.scrollTop = Number(top);
-      }
-    };
-
-    setScrollPosition();
-  }, []);
 
   return (
     <Fragment>
@@ -83,19 +58,23 @@ const Chat = () => {
             <X aria-label="채팅방 숨기기" />
           </button>
         </div>
-        <section className="relative h-full w-full">
-          <div
-            ref={chatRoomRef}
-            className="relative h-full w-full overflow-y-auto rounded-16pxr bg-background pb-15pxr"
-          >
-            <div className="flex flex-1 flex-col ">
-              {DUMMY_MESSAGES.map(({ src, isMyMessage }, index) => (
-                <Message key={`${index}-${src}`} src={src} isMyMessage={isMyMessage} />
-              ))}
-            </div>
+        <div className="relative h-[calc(100%-3.75rem)] w-full rounded-16pxr">
+          <div ref={chatRoomRef} className="flex h-full flex-1 flex-col overflow-y-auto pb-30pxr">
+            {messages.map((message, index) => (
+              <Fragment key={`${index}-${message.nickname}`}>
+                {"image" in message && (
+                  <ZzalMessage
+                    src={message.image}
+                    isMyMessage={false}
+                    nickname={message.nickname}
+                  />
+                )}
+                {"message" in message && <GreetMessage nickname={message.nickname} />}
+              </Fragment>
+            ))}
           </div>
-          <MessagePeek />
-        </section>
+          <MessagePeek onClickSend={handleClickSend} />
+        </div>
       </div>
     </Fragment>
   );
