e7146ca8dd3be78ceaadac98dcf54cc6a7ef2b8d
choi-ik <ci980704@naver.com>
2024-03-06 14:50:53 +0900

    Feat: 이미지 좋아요 mutation 함수 구현

--- src/hooks/api/zzal/useAddImageLike.ts
+++ src/hooks/api/zzal/useAddImageLike.ts
@@ -1,12 +1,25 @@
-import { toast } from "react-toastify";
-import { useMutation } from "@tanstack/react-query";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { addImageLike } from "@/apis/zzal";
+import { GetZzalResponse } from "@/types/zzal.dto";
 
-export const useAddImageLike = () => {
+export const useAddImageLike = (imageIndex: number) => {
+  const queryClient = useQueryClient();
+
   const { mutate, ...rest } = useMutation({
     mutationFn: (imageId: number) => addImageLike(imageId),
-    onSuccess: () => console.log("좋아요 성공"),
-    onError: () => toast.error("좋아요 요청이 실패하였습니다 다시 시도해주십시오."),
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: ["homeZzlas"] });
+
+      const oldData = queryClient.getQueryData(["homeZzals"]) as GetZzalResponse[];
+      const updatedData = [...oldData];
+
+      updatedData[imageIndex] = { ...updatedData[imageIndex], imageLikeYn: true };
+
+      queryClient.setQueryData(["homeZzals"], updatedData);
+
+      return { oldData };
+    },
+    onError: (error, _, rollback) => queryClient.setQueryData(["homeZzals"], rollback?.oldData),
   });
 
   return { addImageLike: mutate, ...rest };
