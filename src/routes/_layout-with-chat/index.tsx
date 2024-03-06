e149be4da7fe5452132210610fd30a7b0fd53b67
choi-ik <ci980704@naver.com>
2024-03-07 02:36:55 +0900

    Feat: 메인 화면으로 리다이렉트하여 받아온 액세스/리프레시 토큰 저장 및 urlParam cleanup

--- src/routes/_layout-with-chat/index.tsx
+++ src/routes/_layout-with-chat/index.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
 import { createFileRoute } from "@tanstack/react-router";
+import { setLocalStorage } from "@/utils/localStorage";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "@/constants/auth";
 
 const Home = () => {
+  useEffect(() => {
+    const currentURL = window.location.href;
+    const urlParams = new URLSearchParams(window.location.search);
+    const accessToken = urlParams.get(ACCESS_TOKEN);
+    const refreshToken = urlParams.get(REFRESH_TOKEN);
+
+    const baseURL = currentURL.split("?")[0];
+
+    if (accessToken && refreshToken) {
+      window.location.href = baseURL;
+
+      setLocalStorage(ACCESS_TOKEN, accessToken);
+      setLocalStorage(REFRESH_TOKEN, refreshToken);
+    }
+  });
   return <div></div>;
 };
 
