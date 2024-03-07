bd6a1c89ba10fddf3164948333a75f16574e4d06
hy219 <jhy_0219@daum.net>
2024-03-07 13:40:10 +0900

    Refactor: tagId를 사용하여 추천 태그의 키 할당
    기존에 index를 사용해서 키를 할당한 부분 제거

--- src/components/common/RecommendTag.tsx
+++ src/components/common/RecommendTag.tsx
@@ -15,8 +15,8 @@ const RecommendTag = ({ title, recommendTags, className }: Props) => {
     <div className={cn("mb-4", className)}>
       <div className="mb-4 text-sm font-bold">{title}</div>
       <ul className="ml-2 flex w-650pxr flex-wrap gap-2 after:block after:w-650pxr after:border-b after:border-black after:content-['']">
-        {recommendTags.map((recommendTag, index) => (
-          <li key={`${index}-${recommendTag}`} className="mb-2">
+        {recommendTags.map((recommendTag) => (
+          <li key={`${recommendTag.tagId}`} className="mb-2">
             <TagBadge content={recommendTag.tagName} isClickable />
           </li>
         ))}
