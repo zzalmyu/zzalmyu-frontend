d6e7dd355913964ed2b18860c15fd1c654caa726
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-03-07 14:13:12 +0900

    Style: TagSearchForm, TagAutoComplete style 수정

--- src/components/common/SearchTag/TagSearchForm.tsx
+++ src/components/common/SearchTag/TagSearchForm.tsx
@@ -50,25 +50,25 @@ const TagSearchForm = ({ className }: Props) => {
   };
 
   return (
-    <div className={cn(`relative flex w-650pxr flex-col flex-wrap `, className)}>
-      <form onSubmit={handleSubmitForm}>
+    <div className={cn(`relative flex min-w-500pxr flex-col flex-wrap p-0`, className)}>
+      <form onSubmit={handleSubmitForm} className="h-full w-full">
         <label htmlFor="tagInput" className="a11y-hidden">
           태그 입력
         </label>
-        <div className="flex max-w-650pxr flex-wrap items-center rounded-3xl border border-black px-4">
+        <div className="flex h-full w-full flex-wrap items-center gap-4 rounded-full border border-gray-300 px-6 shadow-xl">
           <input
             id="tagInput"
             name="tag"
             ref={inputRef}
             onChange={handleChangeInputText}
-            className="min-h-16 flex-1 rounded-xl border-none bg-transparent outline-none"
+            className="z-20 min-h-16 flex-1 rounded-xl border-none bg-transparent outline-none"
           />
-          <button type="submit">
-            <Search aria-label="검색" />
+          <button type="submit" className="z-20 rounded-full bg-primary p-2 text-white">
+            <Search aria-label="검색" size={20} />
           </button>
         </div>
       </form>
-      <div className="absolute top-70pxr flex w-full justify-center">
+      <div className="absolute top-[35px] flex w-full justify-center">
         <Suspense fallback={null}>
           <TagAutoComplete keyword={tagKeyword} onCloseAutoComplete={handleCloseAutoComplete} />
         </Suspense>
