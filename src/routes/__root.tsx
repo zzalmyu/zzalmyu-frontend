aea2ca9b226a44f682103f899fdb2402d91ef00e
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-03-08 19:12:45 +0900

    Refactor: Navigation Footer 컴포넌트화

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,7 @@ import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { FolderUp, Heart, Home, PlusSquare } from "lucide-react";
+import { cn } from "@/utils/tailwind";
 import NotFound from "./-NotFound";
 import Header from "@/components/common/Header";
 
@@ -31,26 +32,31 @@ const menuItems = [
   },
 ];
 
+const NavigationFooter = () => {
+  return (
+    <div
+      className={cn(
+        "absolute bottom-0 flex h-70pxr w-full items-center justify-evenly bg-background text-text-primary transition-transform sm:hidden",
+      )}
+    >
+      {menuItems.map(({ path, Icon, name }) => (
+        <Link to={path} className="flex w-65pxr flex-col items-center gap-1" key={name}>
+          <Icon size={24} strokeWidth={1.5} aria-label={name} />
+          <span className="text-xs font-bold group-hover:text-blue-500">{name}</span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
 const RootComponent = () => {
   return (
     <div className="relative h-screen w-screen">
       <Header />
-      <div className="h-[calc(100vh-4.25rem)] pb-70pxr sm:h-[calc(100vh-4.25rem)]">
+      <div className="h-[calc(100vh-4.25rem)] pb-70pxr sm:pb-0">
         <Outlet />
       </div>
-      <div className="absolute bottom-0 flex h-70pxr w-full items-center justify-evenly bg-background text-text-primary sm:hidden">
-        {menuItems.map(({ path, Icon, name }) => (
-          <Link
-            to={path}
-            className="flex w-65pxr flex-col items-center gap-1 [&.active]:text-white"
-            activeProps={{ className: "bg-transparent" }}
-            key={name}
-          >
-            <Icon size={30} aria-label={name} />
-            <span className="text-xs font-bold group-hover:text-blue-500">{name}</span>
-          </Link>
-        ))}
-      </div>
+      <NavigationFooter />
       <ToastContainer />
       <ReactQueryDevtools buttonPosition="top-right" />
       <TanStackRouterDevtools position="bottom-right" />
