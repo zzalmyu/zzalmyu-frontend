60ed9c0fd2f474bf46fc5832b94ad9671f3da413
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-03-08 19:13:28 +0900

    Style: Chat toggle 버튼 스타일 수정

--- src/routes/_layout-with-chat.tsx
+++ src/routes/_layout-with-chat.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createFileRoute } from "@tanstack/react-router";
 import { useAtom } from "jotai";
+import { MessageCircle } from "lucide-react";
 import { cn } from "@/utils/tailwind";
 import Chat from "@/components/common/Chat";
 import { $isChatOpen } from "@/store/chat";
@@ -14,15 +15,22 @@ const LayoutWithChat = () => {
     <div className="h-full w-full overflow-hidden">
       <div className="relative flex h-80pxr w-full items-center justify-center px-10pxr sm:justify-between sm:px-6 ">
         <div className="hidden w-115pxr self-start text-xl font-bold text-text-primary sm:block">
+          {/* TODO: [2024.03.08]: route별 title로 교체하기 */}
           취향 저격 짤
         </div>
-        <TagSearchForm className="z-10 sm:w-470pxr md:w-full" />
+        <TagSearchForm className="z-10 sm:w-400pxr md:w-550pxr lg:w-full" />
         <button
-          className="btn btn-ghost btn-sm hidden w-115pxr rounded-full outline outline-1 outline-border sm:block"
+          className={cn(
+            "btn btn-ghost btn-sm hidden h-45pxr w-45pxr items-center justify-center rounded-full outline outline-1 outline-border sm:flex md:w-115pxr md:bg-background md:text-text-primary",
+            isChatOpen ? "bg-primary text-white" : "",
+          )}
           onClick={handleClickChatToggleButton}
         >
-          {isChatOpen && "채팅방 숨기기"}
-          {!isChatOpen && "채팅방 보기"}
+          <div className="hidden md:block">
+            {isChatOpen && "채팅방 숨기기"}
+            {!isChatOpen && "채팅방 보기"}
+          </div>
+          <MessageCircle className="block md:hidden" fill="white" strokeWidth={1.5} />
         </button>
       </div>
       <div className="relative flex h-[calc(100%-80px)] w-full overflow-hidden">
@@ -32,13 +40,6 @@ const LayoutWithChat = () => {
             isChatOpen ? "w-[67%]" : "w-full",
           )}
         >
-          <button
-            className="btn btn-ghost btn-sm  w-115pxr rounded-full outline outline-1 outline-border sm:block"
-            onClick={handleClickChatToggleButton}
-          >
-            {isChatOpen && "채팅방 숨기기"}
-            {!isChatOpen && "채팅방 보기"}
-          </button>
           <Outlet />
         </div>
         <Chat />
