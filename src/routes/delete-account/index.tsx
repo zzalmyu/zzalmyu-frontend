690a97ea422caeadae42994ba66fde15c1aac6f3
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-03-09 13:53:52 +0900

    Refactor: 중복되는 스타일 코드 개선

--- src/routes/delete-account/index.tsx
+++ src/routes/delete-account/index.tsx
@@ -8,44 +8,55 @@ import { cn } from "@/utils/tailwind";
 const DeleteAccount = () => {
   const [step, setStep] = useState(1);
 
-  const handleDelete = () => {
+  const handleDeleteAccount = () => {
     toast.success("계정이 삭제되었습니다.");
     // TODO: [2024.03.03] 삭제 로직 추가하기
     setStep(2);
   };
 
+  const buttonWrappingClasses = "flex w-full max-w-950pxr flex-row-reverse my-10pxr sm:my-0";
+  const buttonClasses = "btn m-0 w-auto rounded-full hover:opacity-75 sm:m-10  sm:w-40";
+  const noticeContainerClasses = "max-w-570pxr pb-5 sm:h-385pxr";
+
   const AccountDeletionNotice = () => {
+    const deletionNotices = [
+      {
+        icon: <Folder aria-label="폴더 아이콘" />,
+        text: "짤을 업로드하지 못하고, 업로드할 짤들을 조회하지 못하게 됩니다",
+      },
+      {
+        icon: <Heart aria-label="좋아요 아이콘" />,
+        text: "짤에 좋아요를 추가하지 못하고, 좋아요한 짤들을 조회하지 못하게 됩니다",
+      },
+      {
+        icon: <MessageCircle aria-label="메세지 아이콘" />,
+        text: "다른 사용자들에게 짤 메세지를 전송하지 못하게 됩니다",
+      },
+      {
+        icon: <Wand aria-label="지팡이 아이콘" />,
+        text: "회원님만을 위한 추천 짤 목록을 받지 못하게 됩니다",
+      },
+    ];
+
     return (
-      <Fragment>
-        <div className="flex flex-row gap-4">
-          <Folder aria-label="폴더 아이콘" />
-          <span className=" font-bold">
-            짤을 업로드하지 못하고, 업로드할 짤들을 조회하지 못하게 됩니다
-          </span>
-        </div>
-        <div className="divider divider-neutral "></div>
-        <div className="flex flex-row gap-4">
-          <Heart aria-label="좋아요 아이콘" />
-          <span className=" font-bold">
-            짤에 좋아요를 추가하지 못하고, 좋아요한 짤들을 조회하지 못하게 됩니다
-          </span>
-        </div>
-        <div className="divider divider-neutral "></div>
-        <div className="flex flex-row gap-4">
-          <MessageCircle aria-label="메세지 아이콘" />
-          <span className="  font-bold">다른 사용자들에게 짤 메세지를 전송하지 못하게 됩니다</span>
-        </div>
-        <div className="divider divider-neutral "></div>
-        <div className="flex flex-row gap-4">
-          <Wand aria-label="지팡이 아이콘" />
-          <span className=" font-bold">회원님만을 위한 추천 짤 목록을 받지 못하게 됩니다</span>
-        </div>
-      </Fragment>
+      <div>
+        {deletionNotices.map(({ icon, text }, index) => (
+          <div>
+            <div key={index} className="flex flex-row gap-4">
+              {icon}
+              <span className="font-bold">{text}</span>
+            </div>
+            {index !== deletionNotices.length - 1 && (
+              <div className="divider divider-neutral"></div>
+            )}
+          </div>
+        ))}
+      </div>
     );
   };
 
   return (
-    <div className="flex h-full flex-col items-center p-42pxr">
+    <div className="flex h-full flex-col items-center p-20pxr sm:p-42pxr">
       <div className="flex w-4/6 flex-col font-bold">
         <p className="mb-10pxr text-2xl">계정 삭제</p>
         <div className="breadcrumbs pb-20pxr text-base sm:text-lg">
@@ -61,17 +72,14 @@ const DeleteAccount = () => {
         <div className="flex flex-col items-center">
           {step === 1 && (
             <Fragment>
-              <div className="max-w-570pxr pb-11 pt-8 sm:h-380pxr">
+              <div className={cn("pt-8", noticeContainerClasses)}>
                 <div className="mb-25pxr">
                   <span className="text-xl font-bold">계정을 삭제하시겠어요?</span>
                 </div>
                 <AccountDeletionNotice />
               </div>
-              <div className="flex w-full max-w-950pxr flex-row-reverse">
-                <button
-                  onClick={handleDelete}
-                  className="btn m-0 w-auto rounded-full hover:opacity-75 sm:m-10  sm:w-40"
-                >
+              <div className={buttonWrappingClasses}>
+                <button onClick={handleDeleteAccount} className={buttonClasses}>
                   삭제하기
                 </button>
               </div>
@@ -79,7 +87,7 @@ const DeleteAccount = () => {
           )}
           {step === 2 && (
             <Fragment>
-              <div className="h-full w-full max-w-570pxr pb-11 sm:h-380pxr">
+              <div className={noticeContainerClasses}>
                 <div className="mb-25pxr flex flex-col justify-center gap-3">
                   <CheckCircle2
                     size={50}
@@ -92,11 +100,9 @@ const DeleteAccount = () => {
                 </div>
                 <AccountDeletionNotice />
               </div>
-              <div className="flex w-full max-w-950pxr flex-row-reverse">
+              <div className={buttonWrappingClasses}>
                 <Link to="/">
-                  <button className="btn m-0 w-auto rounded-full hover:opacity-75 sm:m-10  sm:w-40">
-                    닫기
-                  </button>
+                  <button className={buttonClasses}>닫기</button>
                 </Link>
               </div>
             </Fragment>
