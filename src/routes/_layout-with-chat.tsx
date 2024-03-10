287244f1a9b3a99036101793a91c5d14652fd40d
Yongjae Choi <yongjae.choi20@gmail.com>
2024-03-10 17:38:27 +0900

    [#112] Style: v3 layout 적용 (#118)

--- src/routes/_layout-with-chat.tsx
+++ src/routes/_layout-with-chat.tsx
@@ -1,28 +1,47 @@
 import { Outlet, createFileRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useAtomValue } from "jotai";
+import { useAtom } from "jotai";
+import { MessageCircle } from "lucide-react";
 import { cn } from "@/utils/tailwind";
 import Chat from "@/components/common/Chat";
 import { $isChatOpen } from "@/store/chat";
+import TagSearchForm from "@/components/common/SearchTag/TagSearchForm";
 
 const LayoutWithChat = () => {
-  const isChatOpen = useAtomValue($isChatOpen);
-
+  const [isChatOpen, setIsChatOpen] = useAtom($isChatOpen);
+  const handleClickChatToggleButton = () => {
+    setIsChatOpen((prev) => !prev);
+  };
   return (
-    <div className="relative flex h-full w-full overflow-hidden">
-      <div
-        className={cn(
-          "mt-60pxr h-[calc(100%-3.75rem)] overflow-auto border-r border-border transition-[width] duration-500 ease-in-out",
-          isChatOpen ? "w-[67%]" : "w-full",
-        )}
-      >
-        <Outlet />
+    <div className="h-full w-full overflow-hidden">
+      <div className="relative flex h-120pxr w-full flex-col items-center gap-4 border-b border-border px-10pxr pt-10pxr sm:h-150pxr sm:px-6">
+        <TagSearchForm className="z-10 mx-auto sm:w-400pxr md:w-550pxr lg:w-full" />
+        <div className="relative flex w-full flex-1 items-center">
+          <button
+            className={cn(
+              "btn btn-ghost btn-sm absolute right-0 hidden h-45pxr w-45pxr items-center justify-center rounded-full border border-border sm:flex md:w-120pxr md:bg-background md:text-text-primary",
+              isChatOpen ? "bg-primary text-white" : "",
+            )}
+            onClick={handleClickChatToggleButton}
+          >
+            <div className="hidden md:block">
+              {isChatOpen && "채팅방 숨기기"}
+              {!isChatOpen && "채팅방 보기"}
+            </div>
+            <MessageCircle className="block md:hidden" fill="white" strokeWidth={1.5} />
+          </button>
+        </div>
       </div>
-
-      <Chat />
-      <ReactQueryDevtools buttonPosition="top-right" />
-      <TanStackRouterDevtools position="bottom-right" />
+      <div className="relative flex h-[calc(100%-80px)] w-full overflow-hidden">
+        <div
+          className={cn(
+            "h-full overflow-auto border-r border-border px-6 py-4 transition-[width] duration-500 ease-in-out",
+            isChatOpen ? "w-[67%]" : "w-full",
+          )}
+        >
+          <Outlet />
+        </div>
+        <Chat />
+      </div>
     </div>
   );
 };
