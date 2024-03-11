04219e4ef0325e6e5a8ce7aee82d4ec50af925e0
choi-ik <ci980704@naver.com>
2024-03-11 11:00:24 +0900

    Feat: 토큰 재발급 api 구현

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,6 +1,7 @@
-import { getLocalStorage } from "@/utils/localStorage";
+import { getLocalStorage, setLocalStorage } from "@/utils/localStorage";
+import { GetHeaderResponse } from "@/types/auth.dto";
 import http from "./core";
-import { REFRESH_TOKEN } from "@/constants/auth";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "@/constants/auth";
 
 export const patchLogOut = () =>
   http.patch<void>({
@@ -9,3 +10,17 @@ export const patchLogOut = () =>
     },
     url: "/v1/user/logout",
   });
+
+export const postReissueToken = async () => {
+  const { accessToken, refreshToken } = await http.post<GetHeaderResponse>({
+    url: "/v1/user/reissue",
+    headers: {
+      "Authorization-refresh": `Bearer ${getLocalStorage(REFRESH_TOKEN)}`,
+    },
+  });
+
+  setLocalStorage(ACCESS_TOKEN, accessToken.split(" ")[1]);
+  setLocalStorage(REFRESH_TOKEN, refreshToken.split(" ")[1]);
+
+  return accessToken;
+};
