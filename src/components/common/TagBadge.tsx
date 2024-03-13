9c7ef8e1e6ce040f4e5f32afa3a4387deca3c718
Kim-Jaemin420 <jany240@naver.com>
2024-03-13 14:06:40 +0900

    Style: tag badge 글자 크기 및 배치 수정

--- src/components/common/TagBadge.tsx
+++ src/components/common/TagBadge.tsx
@@ -20,7 +20,7 @@ const TagBadge = ({ content, isClickable = false, className }: Props) => {
   return (
     <span
       className={cn(
-        "flex-column text-14px flex w-fit rounded-3xl bg-black p-1 font-bold text-white",
+        "flex-column flex w-fit items-center rounded-3xl bg-black p-1 text-[14px] font-bold text-white",
         className,
       )}
     >
