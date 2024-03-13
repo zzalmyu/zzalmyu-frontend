0a6fbefb0c2f02ab74cbfcf1a7ba0a7007980400
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-03-13 12:28:09 +0900

    Cleanup: 불필요 og 태그 제거

--- src/routes/_layout-with-chat/my-uploaded-zzals/route.tsx
+++ src/routes/_layout-with-chat/my-uploaded-zzals/route.tsx
@@ -7,10 +7,6 @@ const PendingComponent = () => {
       <Helmet>
         <title>업로드한 짤 - 짤뮤니티</title>
         <meta name="description" content="본인이 업로드한 짤들을 확인 및 관리해보세요!" />
-        <meta property="og:title" content="업로드한 짤 - 짤뮤니티" />
-        <meta property="og:image" content="/thumbnail.jpg" />
-        <meta property="og:url" content="https://www.zzalmyu.site/my-uploaded-zzals" />
-        <meta property="og:description" content="본인이 업로드한 짤들을 확인 및 관리해보세요!" />
       </Helmet>
       <div>pending</div>
     </>
