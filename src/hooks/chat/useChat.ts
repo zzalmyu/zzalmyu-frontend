bcafffd0559d5b6b1a4be34b4a04aef7b4d7f36a
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-03-15 11:52:51 +0900

    Modify: WS에 전송되는 메세지를 chat Query cache에 업데이트 하도록 수정

--- src/hooks/chat/useChat.ts
+++ src/hooks/chat/useChat.ts
@@ -1,20 +1,17 @@
-import { useEffect, useRef, useState } from "react";
+import { RefObject, useEffect, useRef } from "react";
 import { toast } from "react-toastify";
 import { CompatClient, Stomp } from "@stomp/stompjs";
 import SockJS from "sockjs-client/dist/sockjs";
 import { useAtomValue } from "jotai";
-import {
-  GreetMessageRequest,
-  GreetMessageResponse,
-  ZzalMessageRequest,
-  ZzalMessageResponse,
-} from "@/types/chat.dto";
+import { InfiniteData, useQueryClient } from "@tanstack/react-query";
+import { GetChatResponse, GreetMessageRequest, ZzalMessageRequest } from "@/types/chat.dto";
 import { $previewImage } from "@/store/chat";
 import { CHANNEL_ID, PUBLISH_DESTINATION, SUBSCRIPTION_DESTINATION } from "@/constants/chat";
 
-const useChat = (handleScrollPosition: () => void) => {
+const useChat = (targetRef: RefObject<HTMLDivElement>) => {
+  const queryClient = useQueryClient();
   const stompRef = useRef<CompatClient | null>(null);
-  const [messages, setMessages] = useState<(GreetMessageResponse | ZzalMessageResponse)[]>([]);
+  // const [messages, setMessages] = useState<(GreetMessageResponse | ZzalMessageResponse)[]>([]);
   const imageSrc = useAtomValue($previewImage);
 
   // TODO: [2024.03.06] 채팅 에러 핸들링 로직 구현
@@ -28,9 +25,28 @@ const useChat = (handleScrollPosition: () => void) => {
       stompRef.current?.subscribe(SUBSCRIPTION_DESTINATION, (frame) => {
         try {
           const parsedMessages = JSON.parse(frame.body);
-          setMessages((currentMessages) => [...currentMessages, parsedMessages]);
+          queryClient.setQueryData<InfiniteData<GetChatResponse>>(["chat"], (oldData) => {
+            if (!oldData) {
+              return undefined;
+            }
+            const oldMessages = oldData.pages.flatMap((page) => page);
+            // const updatedFirstPage = [...oldData.pages[0], parsedMessages];
+            const updatedPages = oldData.pages.map((page, idx) => {
+              if (idx === 0) {
+                return [parsedMessages, ...page];
+              }
+              return page;
+            });
+            console.log(oldData, oldMessages, updatedPages);
+            return { ...oldData, pages: updatedPages } as InfiniteData<GetChatResponse>;
+          });
+          // handleScrollPosition();
+          targetRef.current?.scrollTo(0, targetRef.current.scrollHeight);
         } catch (error) {
+          console.error(error);
           toast.error("채팅 연결 중 예상치 못한 오류가 발생했습니다!");
+        } finally {
+          console.log(JSON.parse(frame.body));
         }
       });
 
@@ -42,7 +58,7 @@ const useChat = (handleScrollPosition: () => void) => {
   const handleSendMessage = (type: "zzal" | "greet") => {
     if (stompRef.current?.connected) {
       const messageContent: GreetMessageRequest | ZzalMessageRequest = {
-        email: "yjc@test.com",
+        email: "cjy@test.com",
         channelId: CHANNEL_ID,
       };
 
@@ -66,11 +82,11 @@ const useChat = (handleScrollPosition: () => void) => {
     };
   }, []);
 
-  useEffect(() => {
-    handleScrollPosition();
-  }, [messages]);
+  // useEffect(() => {
+  //   handleScrollPosition();
+  // }, [messages]);
 
-  return { handleSendMessage, messages };
+  return { handleSendMessage };
 };
 
 export default useChat;
