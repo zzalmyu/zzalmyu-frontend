798c54561fa40f3d646b180e64e27a11ff1b9e03
Choi-ik <ci980704@naver.com>
2024-03-17 22:20:07 +0900

    [#117] Feat: 토큰 재발급 기능 구현 (#123)

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { getLocalStorage } from "@/utils/localStorage";
+import { ReissueTokenResponse } from "@/types/auth.dto";
 import http from "./core";
 import { REFRESH_TOKEN } from "@/constants/auth";
 
@@ -9,6 +11,31 @@ export const patchLogOut = () =>
     },
     url: "/v1/user/logout",
   });
+
+
+export const postReissueToken = async (): Promise<{
+  accessTokenResponse: string;
+  refreshTokenResponse: string;
+}> => {
+  const refreshToken = getLocalStorage(REFRESH_TOKEN);
+
+  const { headers } = await axios.post<ReissueTokenResponse>(
+    `${import.meta.env.VITE_BASE_URL}/v1/user/reissue`,
+    {},
+    {
+      headers: {
+        "Authorization-refresh": `Bearer ${refreshToken}`,
+        "Content-Type": "application/json",
+      },
+      timeout: 10000,
+    },
+  );
+
+  return {
+    accessTokenResponse: headers.authorization.split(" ")[1],
+    refreshTokenResponse: headers["authorization-refresh"].split(" ")[1],
+  };
+};
 
 export const deleteUserWithdraw = () =>
   http.delete<void>({
