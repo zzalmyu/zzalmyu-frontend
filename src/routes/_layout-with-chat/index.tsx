377d4aef599a6f002a7a571d5f69f4f79ab6a5b8
choi-ik <ci980704@naver.com>
2024-03-17 20:28:08 +0900

    Feat: 권한에 따라 홈으로 리다이렉트 한 뒤, 로그인 모달 띄우기 및 로그인 후 이전 페이지로 리다이렉트 기능 구현

--- src/routes/_layout-with-chat/index.tsx
+++ src/routes/_layout-with-chat/index.tsx
@@ -1,26 +1,37 @@
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { createFileRoute, getRouteApi, useNavigate } from "@tanstack/react-router";
-import { setLocalStorage } from "@/utils/localStorage";
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "@/constants/auth";
-
+import { useOverlay } from "@toss/use-overlay";
+import LoginModal from "@/components/LoginModal";
+import { getLocalStorage, setLocalStorage } from "@/utils/localStorage";
+import { ACCESS_TOKEN, REDIRECT_PATH, REFRESH_TOKEN } from "@/constants/auth";
 const route = getRouteApi("/_layout-with-chat/");
 
 const Home = () => {
-  const { accessToken, refreshToken } = route.useSearch<{
+  const { accessToken, refreshToken, redirect } = route.useSearch<{
     accessToken: string;
     refreshToken: string;
+    redirect: string;
   }>();
+
   const navigate = useNavigate();
+  const loginModalOverlay = useOverlay();
 
   useEffect(() => {
     if (accessToken && refreshToken) {
+      const redirectPath = getLocalStorage("redirect") as string;
       setLocalStorage(ACCESS_TOKEN, accessToken);
       setLocalStorage(REFRESH_TOKEN, refreshToken);
+
+      navigate({ to: redirectPath || "/" });
+    }
+    if (redirect) {
+      setLocalStorage(REDIRECT_PATH, redirect);
+      loginModalOverlay.open(({ isOpen, close }) => <LoginModal isOpen={isOpen} onClose={close} />);
       navigate({ to: "/" });
     }
   });
 
-  return <div></div>;
+  return <Fragment></Fragment>;
 };
 
 export const Route = createFileRoute("/_layout-with-chat/")({
