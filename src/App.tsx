8b2ad26d8cea4f8923fc5272db18db71304d32d5
choi-ik <ci980704@naver.com>
2024-03-17 20:15:39 +0900

    Feat: 구현된 Protected Route Context로 RouterProvider 래핑

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { OverlayProvider } from "@toss/use-overlay";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "@/components/Auth";
 import { routeTree } from "./routeTree.gen";
+import { useAuthContext } from "./hooks/auth/useAuthContext";
 
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
+  context: {
+    auth: undefined!,
+  },
 });
 
 declare module "@tanstack/react-router" {
@@ -14,13 +19,20 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const InnerApp = () => {
+  const auth = useAuthContext();
+  return <RouterProvider router={router} context={{ auth }} />;
+};
+
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <OverlayProvider>
-        <RouterProvider router={router} />
+        <AuthProvider>
+          <InnerApp />
+        </AuthProvider>
       </OverlayProvider>
     </QueryClientProvider>
   );
