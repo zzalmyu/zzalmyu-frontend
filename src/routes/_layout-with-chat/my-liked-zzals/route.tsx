09ea5d07f019d7cf4bca6293b637dcd00d3f87f7
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-03-18 00:17:31 +0900

    Refactor: 누락되었던 <>를 Fragment로 수정

--- src/routes/_layout-with-chat/my-liked-zzals/route.tsx
+++ src/routes/_layout-with-chat/my-liked-zzals/route.tsx
@@ -1,15 +1,16 @@
 import { Helmet } from "react-helmet-async";
+import { Fragment } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 
 const PendingComponent = () => {
   return (
-    <>
+    <Fragment>
       <Helmet>
         <title>좋아요한 짤 - 짤뮤니티</title>
         <meta name="description" content="본인이 좋아요한 짤들을 확인 및 관리해보세요!" />
       </Helmet>
       <div>pending my liked zzal</div>
-    </>
+    </Fragment>
   );
 };
 
