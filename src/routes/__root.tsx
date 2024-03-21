ee74b37b5c08e8c2571dea7395c0d4e09c16983d
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-03-21 10:49:14 +0900

    Modify: NavigationFooter 정책 업데이트 반영

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,65 +1,118 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Link, Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { FolderUp, Heart, Home, PlusSquare } from "lucide-react";
+import { Fragment } from "react";
+import { Link, Outlet, createRootRouteWithContext, useNavigate } from "@tanstack/react-router";
+import { CircleUser, FolderUp, Heart, Home, LogOut, Plus } from "lucide-react";
+import { useOverlay } from "@toss/use-overlay";
+import { useAtom, useAtomValue } from "jotai";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthContextType } from "@/components/Auth";
+import LoginModal from "@/components/LoginModal";
 import { cn } from "@/utils/tailwind";
+import { getLocalStorage, setLocalStorage } from "@/utils/localStorage";
 import NotFound from "./-NotFound";
 import Header from "@/components/common/Header";
-
-const menuItems = [
-  {
-    path: "/",
-    Icon: Home,
-    name: "홈",
-    event: "홈_페이지로_이동",
-  },
-  {
-    path: "/my-uploaded-zzals/",
-    Icon: FolderUp,
-    name: "업로드한 짤",
-    event: "업로드한_짤_페이지로_이동",
-  },
-  {
-    path: "/my-liked-zzals/",
-    Icon: Heart,
-    name: "좋아요한 짤",
-    event: "좋아요한_짤_페이지로_이동",
-  },
-
-  {
-    path: "/upload-zzal",
-    Icon: PlusSquare,
-    name: "업로드",
-    event: "짤_업로드_페이지로_이동",
-  },
-];
+import { $userInformation } from "@/store/user";
+import { REDIRECT_PATH, REFRESH_TOKEN } from "@/constants/auth";
+import useLogout from "@/hooks/api/auth/useLogout";
+import { $scrollDirection } from "@/store/scroll";
 
 const handleClickButton = (eventName: string) => () => {
   gtag("event", "page_view", { event_category: eventName });
 };
 
 const NavigationFooter = () => {
+  const loginModalOverlay = useOverlay();
+  const [userInformation, setUserInformation] = useAtom($userInformation);
+  // const { userInformation } = useGetUserInformation();
+  const refreshToken = getLocalStorage(REFRESH_TOKEN);
+  const navigate = useNavigate();
+  const { logout } = useLogout();
+  const role = refreshToken && userInformation ? userInformation.role : "GUEST";
+  const scrollDirection = useAtomValue($scrollDirection);
+
+  const handleClickLogin = () => {
+    setLocalStorage(REDIRECT_PATH, "/");
+    loginModalOverlay.open(({ isOpen, close }) => <LoginModal isOpen={isOpen} onClose={close} />);
+    gtag("event", "modal_open", { event_category: "로그인_모달_띄우기" });
+  };
+
+  const handleClickLogout = () => {
+    logout(undefined, {
+      onSuccess: () => {
+        setUserInformation({
+          userId: 0,
+          email: "",
+          role: "GUEST",
+        });
+        navigate({ to: "/" });
+      },
+    });
+  };
+
   return (
     <div
       className={cn(
-        "absolute bottom-0 flex h-70pxr w-full items-center justify-evenly bg-background text-text-primary transition-transform sm:hidden",
+        "absolute bottom-0 z-10 flex h-60pxr w-full items-center justify-evenly bg-background text-text-primary shadow-[0_-1px_10px_1px_rgba(0,0,0,0.1)] transition-transform sm:hidden",
+        scrollDirection === "up" ? "" : "translate-y-full",
       )}
     >
-      {menuItems.map(({ path, Icon, name, event }) => (
-        <Link
-          to={path}
-          className="flex w-65pxr flex-col items-center gap-1"
-          key={name}
-          activeProps={{ className: "text-primary" }}
-          onClick={handleClickButton(event)}
-        >
-          <Icon size={24} strokeWidth={1.5} aria-label={name} />
-          <span className="text-xs font-bold">{name}</span>
-        </Link>
-      ))}
+      <Link
+        to="/"
+        className="flex w-65pxr flex-col items-center gap-1"
+        activeProps={{ className: "text-primary" }}
+        onClick={handleClickButton("홈_페이지로_이동")}
+      >
+        <Fragment>
+          <Home size={24} strokeWidth={1.5} aria-label="홈" />
+          <span className="text-xs font-bold">홈</span>
+        </Fragment>
+      </Link>
+      <Link
+        to="/my-liked-zzals"
+        className={cn("flex w-65pxr flex-col items-center gap-1", role === "GUEST" ? "hidden" : "")}
+        activeProps={{ className: "text-primary" }}
+        onClick={handleClickButton("좋아요한_짤_페이지로_이동")}
+      >
+        <Fragment>
+          <Heart size={24} strokeWidth={1.5} aria-label="좋아요한 짤" />
+          <span className="text-xs font-bold">좋아요한 짤</span>
+        </Fragment>
+      </Link>
+      <Link
+        to="/upload-zzal"
+        className="flex w-65pxr flex-col items-center gap-1"
+        activeProps={{ className: "text-primary" }}
+        onClick={handleClickButton("짤_업로드_페이지로_이동")}
+      >
+        <div className="flex h-45pxr w-45pxr items-center justify-center rounded-full bg-primary font-bold text-white">
+          <Plus size={24} strokeWidth={2} aria-label="업로드" />
+        </div>
+      </Link>
+      <Link
+        to="/my-uploaded-zzals"
+        className={cn("flex w-65pxr flex-col items-center gap-1", role === "GUEST" ? "hidden" : "")}
+        activeProps={{ className: "text-primary" }}
+        onClick={handleClickButton("업로드한_짤_페이지로_이동")}
+      >
+        <Fragment>
+          <FolderUp size={24} strokeWidth={1.5} aria-label="업로드한 짤" />
+          <span className="text-xs font-bold">업로드한 짤</span>
+        </Fragment>
+      </Link>
+      {role === "GUEST" && (
+        <div onClick={handleClickLogin} className="flex w-65pxr flex-col items-center gap-1">
+          <CircleUser size={24} strokeWidth={1.5} aria-label="" />
+          <span className="text-xs font-bold">로그인</span>
+        </div>
+      )}
+      {role === "USER" && (
+        <div onClick={handleClickLogout} className="flex w-65pxr flex-col items-center gap-1">
+          <LogOut size={24} strokeWidth={1.5} aria-label="" />
+          <span className="text-xs font-bold">로그아웃</span>
+        </div>
+      )}
     </div>
   );
 };
@@ -70,9 +123,9 @@ interface RouterContext {
 
 const RootComponent = () => {
   return (
-    <div className="relative h-screen w-screen">
+    <div className="relative h-screen w-screen overflow-hidden">
       <Header />
-      <div className="h-[calc(100vh-4.25rem)] pb-70pxr sm:pb-0">
+      <div className="h-[calc(100vh-4.25rem)] sm:pb-0">
         <Outlet />
       </div>
       <NavigationFooter />
