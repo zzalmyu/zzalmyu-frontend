0eb8d0cdbc2c0e5271e0eaba6bbfb80aa53f485b
choi-ik <ci980704@naver.com>
2024-04-22 13:43:21 +0900

    Feat: 업로드 짤 페이지 라우팅 파일에 Lazy 적용

---
+++ src/routes/_authentication/upload-zzal/route.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_authentication/upload-zzal")({
+  component: () => <div>Hello /_authentication/upload-zzal!</div>,
+});
