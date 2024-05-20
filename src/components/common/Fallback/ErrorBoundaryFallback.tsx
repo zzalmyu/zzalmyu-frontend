9c55e78f5ada5e53638710f893b9509369b0bdf7
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-05-20 13:28:21 +0900

    Feat: ErrorBoundary 및 Suspense 공통 Fallback 구현

---
+++ src/components/common/Fallback/ErrorBoundaryFallback.tsx
@@ -0,0 +1,17 @@
+import { ErrorBoundaryFallbackProps } from "@suspensive/react";
+
+const ErrorBoundaryFallback = ({ reset }: ErrorBoundaryFallbackProps) => (
+  <div className="flex h-full flex-1 flex-col items-center text-lg">
+    <div role="alert" className=" alert flex flex-col items-center">
+      <div className="font-bold">잠시 후 다시 시도해주세요</div>
+      <p className="text-center text-sm">
+        해당 요청을 처리하는데 <br /> 실패했습니다
+      </p>
+      <button className="btn btn-outline btn-wide" onClick={reset}>
+        다시 시도
+      </button>
+    </div>
+  </div>
+);
+
+export default ErrorBoundaryFallback;
---
+++ src/components/common/Fallback/SuspenseSpinnerFallback.tsx
@@ -0,0 +1,7 @@
+const SuspenseSpinnerFallback = () => (
+  <div className="flex h-full flex-1 items-center justify-center">
+    <span className="loading loading-spinner loading-lg text-primary" />
+  </div>
+);
+
+export default SuspenseSpinnerFallback;
