61e2760bee811dc47ca6af295a1e4a67827cc034
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-07-22 16:12:54 +0900

    Chore: next app router root layout 구성

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import localFont from "next/font/local";
+import type { Metadata } from "next";
+
+const pretendard = localFont({
+  src: "./PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
+export const metadata: Metadata = {
+  title: "짤뮤니티 | 나만을 위한 취향 저격 짤 추천",
+  description: "본인의 취향과 선호도에 따라 선별된 짤 컬렉션을 추천 받아 보세요!",
+  keywords: "짤, 이미지, 밈, 짤 검색, 짤 추천",
+  openGraph: {
+    type: "website",
+    url: "https://www.zzalmyu.asia",
+    title: "짤뮤니티 | 나만을 위한 취향 저격 짤 추천",
+    description: "본인의 취향과 선호도에 따라 선별된 짤 컬렉션을 추천 받아 보세요!",
+  },
+  twitter: {
+    title: "짤뮤니티 | 나만을 위한 취향 저격 짤 추천",
+    description: "본인의 취향과 선호도에 따라 선별된 짤 컬렉션을 추천 받아 보세요!",
+    card: "summary",
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ko" data-theme="light" className={`${pretendard.variable}`}>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6PY1WDK5P6"></script>
+        {/* <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-6PY1WDK5P6");
+    </script> */}
+      </head>
+      <body className={pretendard.className}>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
