0dfc4862939bffff1848c441348f46b2ab3b99a9
choi-ik <ci980704@naver.com>
2024-07-26 18:50:17 +0900

    Refactor: 짤, 인증, 채팅, 태그, 신고 관련 쿼리들을 효율적으로 관리하기 위해 queryOptions 객체를 사용하여 한 곳에서 colocation하는 방식으로 수정

---
+++ src/hooks/api/auth/queryKeyFactories.ts
@@ -0,0 +1,13 @@
+import { queryOptions } from "@tanstack/react-query";
+import { getUserInformation } from "@/apis/auth";
+
+export const authQueries = {
+  all: () => ["auth"] as const,
+  userInformationKey: () => [...authQueries.all(), "userInfomation"] as const,
+  getUserInformation: (refreshToken: string) =>
+    queryOptions({
+      queryKey: [...authQueries.userInformationKey()] as const,
+      queryFn: getUserInformation,
+      enabled: !!refreshToken,
+    }),
+};
---
+++ src/hooks/api/tag/queryKeyFactories.ts
@@ -0,0 +1,46 @@
+import { queryOptions } from "@tanstack/react-query";
+import {
+  getPopularTags,
+  getSearchTag,
+  getTopTagsFromHome,
+  getTopTagsFromLiked,
+  getTopTagsFromUploaded,
+} from "@/apis/tag";
+
+export const tagQueries = {
+  all: () => ["tags"] as const,
+  tagListKey: () => [...tagQueries.all(), "tagList"] as const,
+  getTagList: (tag: string) => {
+    const MAX_TAG_RESPONSE_COUNT = 10;
+
+    return queryOptions({
+      queryKey: [...tagQueries.tagListKey(), tag] as const,
+      queryFn: () => getSearchTag(tag),
+      select: (tags) => tags.filter((_tag, index) => index < MAX_TAG_RESPONSE_COUNT),
+    });
+  },
+  popularTagsKey: () => [...tagQueries.all(), "popularTags"] as const,
+  getPopularTags: () =>
+    queryOptions({
+      queryKey: [...tagQueries.popularTagsKey()] as const,
+      queryFn: getPopularTags,
+    }),
+  topTagsFromHomeKey: () => [...tagQueries.all(), "topTagsFromHome"] as const,
+  getTopTagsFromHome: () =>
+    queryOptions({
+      queryKey: [...tagQueries.topTagsFromHomeKey()] as const,
+      queryFn: getTopTagsFromHome,
+    }),
+  topTagsFromLikedKey: () => [...tagQueries.all(), "topTagsFromLiked"] as const,
+  getTopTagsFromLiked: () =>
+    queryOptions({
+      queryKey: [...tagQueries.topTagsFromLikedKey()] as const,
+      queryFn: getTopTagsFromLiked,
+    }),
+  topTagsFromUploadedKey: () => [...tagQueries.all(), "topTagsFromUploaded"] as const,
+  getTopTagsFromUploaded: () =>
+    queryOptions({
+      queryKey: [...tagQueries.topTagsFromUploadedKey()] as const,
+      queryFn: getTopTagsFromUploaded,
+    }),
+};
