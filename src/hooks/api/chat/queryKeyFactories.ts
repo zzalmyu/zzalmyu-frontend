96704b56f6d69526cec8b811a8b47e8e3ed5b4c6
Ik <ci980704@naver.com>
2024-07-29 23:46:20 +0900

    [#249] Refactor: 쿼리들을 효율적으로 관리할 수 있도록 queryOptions 객체를 사용하여 co-location 하는 방식으로 리팩토링 (#252)

---
+++ src/hooks/api/chat/queryKeyFactories.ts
@@ -0,0 +1,24 @@
+import { infiniteQueryOptions } from "@tanstack/react-query";
+import { getChat } from "@/apis/chat";
+
+export const chatQueries = {
+  all: ["chat"] as const,
+  chatListKey: () => [...chatQueries.all, "chatList"] as const,
+  getChatList: () =>
+    infiniteQueryOptions({
+      queryKey: [...chatQueries.chatListKey()] as const,
+      queryFn: async ({ pageParam = 0 }) => await getChat(pageParam),
+      getNextPageParam: (lastPage, _allPages, lastPageParam) => {
+        if (lastPage.length === 0) return;
+
+        return lastPageParam + 1;
+      },
+      select: (data) => ({
+        pages: [...data.pages].reverse(),
+        pageParams: [...data.pageParams],
+      }),
+      initialPageParam: 0,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    }),
+};
