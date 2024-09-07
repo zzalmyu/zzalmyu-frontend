22fe4d464847869719cfb0847e327ee1abb5316d
choi-ik <ci980704@naver.com>
2024-09-07 19:59:44 +0900

    Feat: report  관련 데이터와 해당 데이터를 반환하도록 mocking handler 구현

---
+++ src/mocks/data/reports.ts
@@ -0,0 +1,16 @@
+import { Report } from "@/types/report";
+
+export const reports: Report[] = [
+  {
+    imageId: 2,
+    imageTitle: "박명수",
+    lastReportAt: "마지막 신고 시간",
+    reportCount: 3,
+  },
+  {
+    imageId: 1,
+    imageTitle: "무한도전",
+    lastReportAt: "마지막 신고 시간",
+    reportCount: 3,
+  },
+];
