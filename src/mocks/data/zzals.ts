eff9bfe9c239a6217a810570e2441bd716c183f9
choi-ik <ci980704@naver.com>
2024-09-07 19:59:05 +0900

    Feat: zzal 관련 데이터와 해당 데이터를 반환하도록 mocking handler 구현

---
+++ src/mocks/data/zzals.ts
@@ -0,0 +1,64 @@
+import { GetMyHomeZzalsResponse } from "@/types/zzal.dto";
+
+export const zzals: GetMyHomeZzalsResponse[] = [
+  {
+    imageId: 1,
+    path: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvo_l4pc8yVopI9mR1zfXgkNW-GVzLFoiRFQ&s",
+    title: "무한도전",
+    imageLikeYn: false,
+  },
+  {
+    imageId: 2,
+    path: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ-9ZPc2OfHNKAwQlV8CeHEPgJID-2ba5l7Q&s",
+    title: "박명수",
+    imageLikeYn: false,
+  },
+  {
+    imageId: 3,
+    path: "https://mblogthumb-phinf.pstatic.net/MjAyMTAxMDVfMjE5/MDAxNjA5ODA0NDYxODAx.ieWcq6luZGB9apxLjS_uh-ROYCQS61ubqCW_pA2fTrMg.Zx56vtprvTOoc0sn9oy7eyWGj8TXXlpClSEoWFehQnMg.PNG.bluelaz1103/SE-c3e732d3-5ceb-423f-bbb7-26cf288fe3e0.png?type=w800",
+    title: "강아지",
+    imageLikeYn: false,
+  },
+  {
+    imageId: 4,
+    path: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_tNt-_ForndzbTEayUOp557zR2mSIfzVhVw&s",
+    title: "루피",
+    imageLikeYn: false,
+  },
+  {
+    imageId: 5,
+    path: "https://sitem.ssgcdn.com/87/70/47/item/1000026477087_i1_750.jpg",
+    title: "짱구",
+    imageLikeYn: false,
+  },
+  {
+    imageId: 6,
+    path: "https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/06/25/3370abfe-c3fd-4371-954a-7ac2d6a392bd.jpg",
+    title: "유재석",
+    imageLikeYn: false,
+  },
+  {
+    imageId: 7,
+    path: "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99F71B435D0E28C708",
+    title: "분노",
+    imageLikeYn: false,
+  },
+  {
+    imageId: 8,
+    path: "https://i.namu.wiki/i/9or_SWMZZwADwmILqRyK_50R_-h1SsI7UApSl0-k-u5JP_aXhXvHgCfwMb5RVnNih09z6usjw2ltPdjsxeQCIQ.webp",
+    title: "맹구",
+    imageLikeYn: false,
+  },
+  {
+    imageId: 9,
+    path: "https://i.namu.wiki/i/9or_SWMZZwADwmILqRyK_50R_-h1SsI7UApSl0-k-u5JP_aXhXvHgCfwMb5RVnNih09z6usjw2ltPdjsxeQCIQ.webp",
+    title: "신남",
+    imageLikeYn: false,
+  },
+  {
+    imageId: 10,
+    path: "https://www.focuscolorado.net/bbs/download.php?table=bbs_11&savefilename=bbs_11_5860.jpg&filename=%EA%B0%9C%EC%A1%B8%EB%A6%BC.jpg",
+    title: "졸림",
+    imageLikeYn: false,
+  },
+];
---
+++ src/mocks/handlers/zzalHandlers.ts
@@ -0,0 +1,96 @@
+import { http, HttpResponse } from "msw";
+import { GetMyHomeZzalsResponse } from "@/types/zzal.dto";
+import { zzals } from "../data/zzals";
+
+const ZZAL_BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/v1/image`;
+
+export const zzalHandlers = [
+  http.get(`${ZZAL_BASE_URL}`, ({ request }) => {
+    const url = new URL(request.url);
+    const tag = url.searchParams.get("tagName") as string;
+
+    return HttpResponse.json<GetMyHomeZzalsResponse[]>(getZzals(tag));
+  }),
+  http.get(`${ZZAL_BASE_URL}/like`, ({ request }) => {
+    const url = new URL(request.url);
+    const tag = url.searchParams.get("tagName") as string;
+
+    return HttpResponse.json<GetMyHomeZzalsResponse[]>(getZzals(tag));
+  }),
+  http.get(`${ZZAL_BASE_URL}/upload`, ({ request }) => {
+    const url = new URL(request.url);
+    const tag = url.searchParams.get("tagName") as string;
+
+    return HttpResponse.json<GetMyHomeZzalsResponse[]>(getZzals(tag));
+  }),
+  http.get(`${ZZAL_BASE_URL}/:imageId`, ({ params }) => {
+    const { imageId } = params;
+
+    return HttpResponse.json(getZzal(Number(imageId)));
+  }),
+  http.post(`${ZZAL_BASE_URL}/:imageId/like`, ({ request }) => {
+    const url = new URL(request.url);
+    const imgId = url.searchParams.get("imageId");
+
+    zzalAddLike(Number(imgId));
+
+    return HttpResponse.json({ status: 200 });
+  }),
+  http.post(`${ZZAL_BASE_URL}/:imageId/like/cancel`, ({ request }) => {
+    const url = new URL(request.url);
+    const imgId = url.searchParams.get("imageId");
+
+    zzalCancelLike(Number(imgId));
+
+    return HttpResponse.json({ status: 200 });
+  }),
+  http.post(ZZAL_BASE_URL, async ({ request }) => {
+    const data = await request.formData();
+    const file = data.get("file");
+
+    if (!file) {
+      return new HttpResponse("Missing document", { status: 400 });
+    }
+
+    if (!(file instanceof File)) {
+      return new HttpResponse("Uploaded document is not a File", {
+        status: 400,
+      });
+    }
+
+    return HttpResponse.json({ status: 200 });
+  }),
+];
+
+const getZzals = (tag: string) => {
+  const regex = new RegExp(tag, "i");
+
+  return zzals.filter(({ title }) => regex.test(title));
+};
+
+const getZzal = (id: number) => {
+  const zzal = zzals.filter(({ imageId }) => imageId === id);
+  return {
+    ...zzal,
+    imageTitle: zzal[0].title,
+    uploadUserId: 1,
+    imgUrl: zzal[0].path,
+    tags: [],
+  };
+};
+
+const zzalAddLike = (id: number) => {
+  zzals.forEach(({ imageId }, index) => {
+    if (imageId === id) {
+      zzals[index].imageLikeYn = true;
+    }
+  });
+};
+
+const zzalCancelLike = (id: number) => {
+  zzals.forEach(({ imageId }, index) => {
+    if (imageId === id) {
+      zzals[index].imageLikeYn = false;
+    }
+  });
+};
