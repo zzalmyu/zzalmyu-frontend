6403ab017d79c4667c6c63e7e31aaaab9e449c2d
Yongjae Choi <yongjae.choi20@gmail.com>
2024-09-09 17:54:41 +0900

    [#261] Fix: 서버에서 localStorage 인식 안됨 버그 해결 (#262)

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,12 +1,15 @@
 export const getLocalStorage = (key: string) => {
   try {
-    const value = localStorage.getItem(key);
+    if (typeof window !== "undefined") {
+      const value = localStorage.getItem(key);
 
-    if (!value) {
-      return null;
+      if (!value) {
+        return null;
+      }
+
+      return JSON.parse(value);
     }
-
-    return JSON.parse(value);
+    return null;
   } catch (error) {
     console.error(`Error parsing localStorage item with key "${key}":`, error);
   }
@@ -14,7 +17,9 @@ export const getLocalStorage = (key: string) => {
 
 export const setLocalStorage = <T>(key: string, value: T) => {
   try {
-    localStorage.setItem(key, JSON.stringify(value));
+    if (typeof window !== "undefined") {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
   } catch (error) {
     console.error(`Error serializing value for localStorage item with key "${key}":`, error);
   }
@@ -22,7 +27,9 @@ export const setLocalStorage = <T>(key: string, value: T) => {
 
 export const removeLocalStorage = (key: string) => {
   try {
-    localStorage.removeItem(key);
+    if (typeof window !== "undefined") {
+      localStorage.removeItem(key);
+    }
   } catch (error) {
     console.error(`Failed to remove item from localStorage with key "${key}". Error: ${error}`);
   }
