dfd7df03de310d50ef183961b54532eb2bbfa4a9
Ik <ci980704@naver.com>
2024-09-24 19:42:25 +0900

    [#264] Fix: 구현되지 않은 authHandler를 import 함으로써 생기는 버그 수정 (#265)

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,13 +1,6 @@
-import { authHandlers } from "./authHandlers";
 import { chatHandlers } from "./chatHandlers";
 import { reportHandlers } from "./reportHandlers";
 import { TagHandlers } from "./tagHandlers";
 import { zzalHandlers } from "./zzalHandlers";
 
-export const handler = [
-  ...TagHandlers,
-  ...zzalHandlers,
-  ...reportHandlers,
-  ...chatHandlers,
-  ...authHandlers,
-];
+export const handler = [...TagHandlers, ...zzalHandlers, ...reportHandlers, ...chatHandlers];
